a7b6bf2701463ccb7cbeaaba256e065e5defa3ca
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-03-06 18:14:35 -0300

    Test combat

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,24 +1,30 @@
+class_name Player
 extends CharacterBody2D
 
 @export_range(10, 100000, 10) var WALK_SPEED: float = 300.0
 @export_range(10, 100000, 10) var RUN_SPEED: float = 500.0
+@export_range(10, 100000, 10) var FIGHTING_SPEED: float = 100.0
 @export_range(10, 100000, 10) var STOPPING_STEP: float = 1500.0
 @export_range(10, 100000, 10) var MOVING_STEP: float = 4000.0
 
 @onready var player_interaction: PlayerInteraction = %PlayerInteraction
+@onready var player_fighting: PlayerFighting = %PlayerFighting
+var axis: Vector2
 
 func _process(delta: float) -> void:
 	if player_interaction.is_interacting:
 		velocity = Vector2.ZERO
 		return
 
-	var axis:Vector2 = Vector2(
+	axis = Vector2(
 		Input.get_axis(&"ui_left", &"ui_right"),
 		Input.get_axis(&"ui_up", &"ui_down"),
 	)
 
 	var speed: float
-	if Input.is_action_pressed(&"running"):
+	if player_fighting.is_fighting:
+		speed = FIGHTING_SPEED
+	elif Input.is_action_pressed(&"running"):
 		speed = RUN_SPEED
 	else:
 		speed = WALK_SPEED
--- scenes/player/player_sprite.gd
+++ scenes/player/player_sprite.gd
@@ -1,17 +1,43 @@
 extends AnimatedSprite2D
 
-@export var character: CharacterBody2D
+@export var player: Player
+
+@onready var player_fighting: PlayerFighting = %PlayerFighting
+var angle: float = 0
 
 func _ready() -> void:
-	if not character and owner is CharacterBody2D:
-		character = owner
+	if not player and owner is Player:
+		player = owner
+
+func _process_fighting() -> void:
+	var current_frame = 0
+	var current_frame_progress = 0
+	if animation in [&"action side A", &"action up A", &"action down A"]:
+		current_frame = frame
+		current_frame_progress = frame_progress
+	flip_h = abs(angle) > PI/2
+	if abs(-PI/2 - angle) <= PI/3.9:
+		play(&"action up A")
+	elif abs(PI/2 - angle) <= PI/3.9:
+		play(&"action down A")
+	elif abs(PI - angle) <= PI/3.9:
+		play(&"action side A")
+	elif abs(angle) <= PI/3.9:
+		play(&"action side A")
+		
+	set_frame_and_progress(current_frame, current_frame_progress)
 
 func _process(_delta: float) -> void:
-	if not character:
+	if not player:
 		return
-	if character.velocity.is_zero_approx():
+	if not player.axis.is_zero_approx():
+		angle = player.axis.angle()
+	if player_fighting.is_fighting:
+		_process_fighting()
+		return
+	if player.velocity.is_zero_approx():
 		play(&"idle")
 	else:
-		if not is_zero_approx(character.velocity.x):
-			flip_h = character.velocity.x < 0
+		if not is_zero_approx(player.velocity.x):
+			flip_h = player.velocity.x < 0
 		play(&"walk")
