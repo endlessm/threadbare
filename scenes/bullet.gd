7e6fcea8bcc21cd89833ce90ae76ef953f3b5051
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-03-07 11:59:38 -0300

    WIP

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,12 +1,13 @@
 extends RigidBody2D
 
 func _ready() -> void:
-	var impulse = Vector2(-100, 0).rotated(randf_range(-PI/8, PI/8))
+	var impulse = Vector2(0, -100).rotated(randf_range(-PI/16, PI/16))
 	apply_impulse(impulse)
 
 
 func _on_body_entered(body: Node2D) -> void:
-	# FIXME if body is the shield
 	var impulse = global_position - body.global_position
 	linear_velocity = Vector2.ZERO
 	apply_impulse(impulse)
+	if body.owner is Enemy:
+		body.owner.got_hit()
