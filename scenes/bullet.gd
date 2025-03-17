a747a907cec67e2d1eb5121e57f76dbc437e70f5
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-03-17 13:42:41 -0300

    WIP

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,9 +1,17 @@
 extends RigidBody2D
 
+@onready var sprite_2d: Sprite2D = %Sprite2D
+
 func _ready() -> void:
 	var impulse = Vector2(0, -100).rotated(randf_range(-PI/16, PI/16))
+	sprite_2d.rotate(impulse.angle())
+
 	apply_impulse(impulse)
 
+func _process(delta: float) -> void:
+	sprite_2d.rotation = linear_velocity.angle()
+	# sprite_2d.rotate()
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	var hit_vector = global_position - body.global_position
