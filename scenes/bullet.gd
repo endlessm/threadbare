1cd3a94e62649e9038354f5f1981ffc41c788889
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-03-18 08:58:07 -0300

    WIP

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,9 +1,29 @@
 extends RigidBody2D
 
+enum INK_COLOR_NAMES {
+		CYAN = 0,
+		MAGENTA = 1,
+		YELLOW = 2,
+		BLACK = 3,
+}
+
+const INK_COLORS = {
+		INK_COLOR_NAMES.CYAN: Color(0., 1., 1.),
+		INK_COLOR_NAMES.MAGENTA: Color(1., 0., 1.),
+		INK_COLOR_NAMES.YELLOW: Color(1., 1., 0.),
+		INK_COLOR_NAMES.BLACK: Color(0.2, 0.2, 0.2),
+}
+
+# @export var ink_color: Color = INK_COLORS[INK_COLOR_NAMES.CYAN]
+
+@export_enum("Cyan", "Magenta", "Yellow", "Black") var ink_color_name: int = 0
+
 @onready var visible_things: Node2D = %VisibleThings
 
 
 func _ready() -> void:
+	var color: Color = INK_COLORS[ink_color_name]
+	visible_things.modulate = color
 	var impulse = Vector2(0, -60).rotated(randf_range(-PI/16, PI/16))
 	apply_impulse(impulse)
 
