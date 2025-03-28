d4a4171c718630146a068480725e544993d42ce5
Will Thompson <wjt@endlessos.org>
2025-03-28 12:00:27 +0000

    Add MPL-2.0 license header to all source files

--- scenes/stealth_level/stealth_quest.gd
+++ scenes/stealth_level/stealth_quest.gd
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
 extends Node2D
 @onready var enemy_guards: Node2D = $EnemyGuards
 @onready var transition_fade: ColorRect = %TransitionFade
