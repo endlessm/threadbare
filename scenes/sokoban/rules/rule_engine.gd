f71677a51d96af0831f8e841df1bcdbb3a4930db
Phoenix <68404504+PhoenixStroh@users.noreply.github.com>
2025-04-08 09:52:36 -0400

    Added copyright metatext on top of scripts.

--- scenes/sokoban/rules/rule_engine.gd
+++ scenes/sokoban/rules/rule_engine.gd
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
 extends Node
 
 @export var directional_input: DirectionalInput
@@ -7,157 +9,181 @@ extends Node
 @export var player_groups: PieceGroups
 @export var rules: Array[SokobanRule]
 
+
 func _ready() -> void:
-    directional_input.input = _directional_input
+	directional_input.input = _directional_input
+
 
 func _directional_input(direction_2d: Vector2i) -> bool:
-    _take_turn(direction_2d)
-    return true
+	_take_turn(direction_2d)
+	return true
+
 
 func _take_turn(direction_2d: Vector2i) -> void:
-    var move_attempts : Array[MoveAttempt] = []
-    
-    # collect all move attempts from player pieces
-    for player in board.get_pieces(player_groups):
-        move_attempts.append( _make_move_attempt(player, direction_2d) )
-    
-    print(move_attempts)
+	var move_attempts: Array[MoveAttempt] = []
 
-    # run rules here
-    for rule in rules:
-        for piece in board.get_pieces():
-            _attempt_rule_on_piece(rule, piece, move_attempts)
+	# collect all move attempts from player pieces
+	for player in board.get_pieces(player_groups):
+		move_attempts.append(_make_move_attempt(player, direction_2d))
 
-    # do movement here
-    var total_checked_attempts: Array[MoveAttempt] = []
-    
-    # disable moving pieces to make it easier to get pieces
-    for attempt in move_attempts:
-        attempt.piece.active = false
+	print(move_attempts)
 
-    for attempt in move_attempts:
-        if attempt in total_checked_attempts: continue
+	# run rules here
+	for rule in rules:
+		for piece in board.get_pieces():
+			_attempt_rule_on_piece(rule, piece, move_attempts)
 
-        var checked_attempts : Array[MoveAttempt] = []
-        var result := attempt.can_move(board, move_attempts, checked_attempts)
+	# do movement here
+	var total_checked_attempts: Array[MoveAttempt] = []
 
-        for checked in checked_attempts:
-            if result:
-                checked.apply()
-        
-        total_checked_attempts.append_array(checked_attempts)
+	# disable moving pieces to make it easier to get pieces
+	for attempt in move_attempts:
+		attempt.piece.active = false
 
-    for attempt in move_attempts:
-        attempt.piece.active = true
+	for attempt in move_attempts:
+		if attempt in total_checked_attempts:
+			continue
 
-    # run late rules here
+		var checked_attempts: Array[MoveAttempt] = []
+		var result := attempt.can_move(board, move_attempts, checked_attempts)
 
-    # commands are run here
+		for checked in checked_attempts:
+			if result:
+				checked.apply()
 
-func _attempt_rule_on_piece(rule: SokobanRule, piece: Piece2D, move_attempts: Array[MoveAttempt]) -> void:
-    if not rule.match_pattern: return
-    if rule.match_pattern.size() == 0: return
-    
-    # early check
-    if not _is_fragment_matching(rule.match_pattern[0], piece, move_attempts):
-        return
+		total_checked_attempts.append_array(checked_attempts)
 
-    var pieces : Array[Piece2D] = [piece]
+	for attempt in move_attempts:
+		attempt.piece.active = true
 
-    # check for the rest of pattern in rule direction if any fragment misses, fail entire check
-    var direction_vector := rule.get_vector()
-    for i in range( 1, rule.match_pattern.size() ):
-        var fragment := rule.match_pattern[i]
+	# run late rules here
 
-        var next_pieces := board.get_pieces_at(piece.grid_position + direction_vector * i)
-        
-        var result := false
-        for next in next_pieces:
-            if _is_fragment_matching(fragment, next, move_attempts):
-                result = true
-                pieces.append(next)
-                break
-        if result == false:
-            return
+	# commands are run here
 
-    # rule matches! apply the rule now
-    print("rule matches!")
-    for i in rule.match_pattern.size():
-        var fragment := rule.replace_pattern[i]
 
-        if i < pieces.size():
-            _apply_fragment(fragment, pieces[i], move_attempts)
+func _attempt_rule_on_piece(
+	rule: SokobanRule, piece: Piece2D, move_attempts: Array[MoveAttempt]
+) -> void:
+	if not rule.match_pattern:
+		return
+	if rule.match_pattern.size() == 0:
+		return
 
-func _apply_fragment(fragment: RuleFragment, piece: Piece2D, move_attempts: Array[MoveAttempt]) -> void:
-    match fragment.move_context:
-        RuleFragment.MOVE.ANY:
-            return
-        RuleFragment.MOVE.STILL:
-            for attempt in move_attempts:
-                if attempt.piece == piece:
-                    move_attempts.erase(attempt)
-                    break
-        _:
-            for attempt in move_attempts:
-                if attempt.piece == piece:
-                    move_attempts.erase(attempt)
-                    break
-            
-            move_attempts.append( _make_move_attempt(piece, fragment.get_vector() ) )
+	# early check
+	if not _is_fragment_matching(rule.match_pattern[0], piece, move_attempts):
+		return
 
-func _is_fragment_matching(fragment: RuleFragment, piece: Piece2D, move_attempts: Array[MoveAttempt]) -> bool:
-    if not piece.groups.has_group(fragment.group): return false
+	var pieces: Array[Piece2D] = [piece]
 
-    if fragment.move_context != RuleFragment.MOVE.ANY:
-        for attempt in move_attempts:
-            if attempt.piece == piece:
-                return fragment.is_matching_movement(attempt.new_grid_position - attempt.old_grid_position)
-    
-    return true
+	# check for the rest of pattern in rule direction if any fragment misses, fail entire check
+	var direction_vector := rule.get_vector()
+	for i in range(1, rule.match_pattern.size()):
+		var fragment := rule.match_pattern[i]
+
+		var next_pieces := board.get_pieces_at(piece.grid_position + direction_vector * i)
+
+		var result := false
+		for next in next_pieces:
+			if _is_fragment_matching(fragment, next, move_attempts):
+				result = true
+				pieces.append(next)
+				break
+		if result == false:
+			return
+
+	# rule matches! apply the rule now
+	print("rule matches!")
+	for i in rule.match_pattern.size():
+		var fragment := rule.replace_pattern[i]
+
+		if i < pieces.size():
+			_apply_fragment(fragment, pieces[i], move_attempts)
+
+
+func _apply_fragment(
+	fragment: RuleFragment, piece: Piece2D, move_attempts: Array[MoveAttempt]
+) -> void:
+	match fragment.move_context:
+		RuleFragment.MOVE.ANY:
+			return
+		RuleFragment.MOVE.STILL:
+			for attempt in move_attempts:
+				if attempt.piece == piece:
+					move_attempts.erase(attempt)
+					break
+		_:
+			for attempt in move_attempts:
+				if attempt.piece == piece:
+					move_attempts.erase(attempt)
+					break
+
+			move_attempts.append(_make_move_attempt(piece, fragment.get_vector()))
+
+
+func _is_fragment_matching(
+	fragment: RuleFragment, piece: Piece2D, move_attempts: Array[MoveAttempt]
+) -> bool:
+	if not piece.groups.has_group(fragment.group):
+		return false
+
+	if fragment.move_context != RuleFragment.MOVE.ANY:
+		for attempt in move_attempts:
+			if attempt.piece == piece:
+				return fragment.is_matching_movement(
+					attempt.new_grid_position - attempt.old_grid_position
+				)
+
+	return true
+
 
 func _make_move_attempt(piece: Piece2D, movement: Vector2i) -> MoveAttempt:
-    return MoveAttempt.new(piece, piece.grid_position, piece.grid_position + movement)
+	return MoveAttempt.new(piece, piece.grid_position, piece.grid_position + movement)
+
 
 class MoveAttempt:
-    var piece: Piece2D
-    var old_grid_position: Vector2i
-    var new_grid_position: Vector2i
+	var piece: Piece2D
+	var old_grid_position: Vector2i
+	var new_grid_position: Vector2i
 
-    func _init(i_piece: Piece2D, i_old_grid_position: Vector2i, i_new_grid_position: Vector2i) -> void:
-        piece = i_piece
-        old_grid_position = i_old_grid_position
-        new_grid_position = i_new_grid_position
+	func _init(
+		i_piece: Piece2D, i_old_grid_position: Vector2i, i_new_grid_position: Vector2i
+	) -> void:
+		piece = i_piece
+		old_grid_position = i_old_grid_position
+		new_grid_position = i_new_grid_position
 
-    func can_move(board: Board2D, other_attempts: Array[MoveAttempt], checked_attempts: Array[MoveAttempt]) -> bool:
-        # check for infinite loop
-        if self in checked_attempts:
-            return false
-        else:
-            checked_attempts.append(self)
+	func can_move(
+		board: Board2D, other_attempts: Array[MoveAttempt], checked_attempts: Array[MoveAttempt]
+	) -> bool:
+		# check for infinite loop
+		if self in checked_attempts:
+			return false
 
-        # check if colliding with non-moving tiles
-        if board.get_pieces_at(new_grid_position, null, piece.layer).size() != 0:
-            return false
+		checked_attempts.append(self)
 
-        # check if colliding with moving tiles
-        for other in other_attempts:
-            # make sure you don't check yourself
-            if self == other:
-                continue
-            
-            # if matching new positions, check if the other attempt can move recursively
-            if new_grid_position == other.new_grid_position:
-                return other.can_move(board, other_attempts, checked_attempts)
+		# check if colliding with non-moving tiles
+		if board.get_pieces_at(new_grid_position, null, piece.layer).size() != 0:
+			return false
 
-        return true
+		# check if colliding with moving tiles
+		for other in other_attempts:
+			# make sure you don't check yourself
+			if self == other:
+				continue
 
-    func revert() -> void:
-        if piece:
-            piece.grid_position = old_grid_position
+			# if matching new positions, check if the other attempt can move recursively
+			if new_grid_position == other.new_grid_position:
+				return other.can_move(board, other_attempts, checked_attempts)
 
-    func apply() -> void:
-        if piece:
-            piece.grid_position = new_grid_position
-    
-    func _to_string() -> String:
-        return "Piece %s: %s -> %s" % [piece.name, old_grid_position, new_grid_position]
+		return true
+
+	func revert() -> void:
+		if piece:
+			piece.grid_position = old_grid_position
+
+	func apply() -> void:
+		if piece:
+			piece.grid_position = new_grid_position
+
+	func _to_string() -> String:
+		return "Piece %s: %s -> %s" % [piece.name, old_grid_position, new_grid_position]
