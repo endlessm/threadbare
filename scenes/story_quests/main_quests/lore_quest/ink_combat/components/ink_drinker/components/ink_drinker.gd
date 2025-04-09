1dce1544505084e8c406d1bff0e0381331bbf464
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-04-09 11:17:56 -0300

    Reorganize project files
    Based on what was discussed in https://github.com/endlessm/threadbare/issues/75, the changes are:
    
    In scenes/, have the folders
    
     - story_quests, where we will store the playable levels that make up each story quest.
    
     - world_map, here will live the scenes that make up the open world of the game.
    
     - game_elements, reusable elements (enemies, npcs, props) that are the building blocks of any playable scene.
    
     - menues (pause, main_screen, credits, etc) are the scenes that aren't playable and also are mainly UI centric.
    
     - ui_elements, the building blocks of the menues scenes, and also extradiegetic elements like huds.
    
     - globals, nodes that live outside of any scene but are needed for the game to work.
    
    Also, another decision taken was to have the .tscn files apart from the other files that make up a scene (script files, assets, resources, etc). The idea behind this is that we want learners to be able to quickly figure out what's the 'ready to use' file that they can drag to the editor.

---
+++ scenes/story_quests/main_quests/lore_quest/ink_combat/components/ink_drinker/components/ink_drinker.gd
@@ -0,0 +1,66 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+class_name InkDrinker
+extends CharacterBody2D
+
+const INK_BLOB: PackedScene = preload(
+	"res://scenes/story_quests/main_quests/lore_quest/ink_combat/components/ink_blob/ink_blob.tscn"
+)
+
+@export var autostart: bool = false
+@export var odd_shoot: bool = false
+@export var ink_follows_player: bool = false
+@export_range(10., 100., 5., "or_greater", "or_less", "suffix:m/s") var ink_speed: float = 30.0
+
+@onready var timer: Timer = %Timer
+
+@onready var ink_blob_marker: Marker2D = %InkBlobMarker
+@onready var hit_box: Area2D = %HitBox
+@onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+
+func _ready() -> void:
+	if autostart:
+		start()
+
+
+func _on_timeout() -> void:
+	var player: Player = get_tree().get_first_node_in_group("player")
+	if not is_instance_valid(player):
+		return
+	animated_sprite_2d.play(&"attack anticipation")
+	await animated_sprite_2d.animation_looped
+	animated_sprite_2d.play(&"attack")
+	var ink_blob: InkBlob = INK_BLOB.instantiate()
+	ink_blob.direction = ink_blob_marker.global_position.direction_to(player.global_position)
+	ink_blob.ink_color_name = randi_range(0, 3) as InkBlob.InkColorNames
+	ink_blob.global_position = (ink_blob_marker.global_position + ink_blob.direction * 20.)
+	if ink_follows_player:
+		ink_blob.node_to_follow = player
+	ink_blob.speed = ink_speed
+	get_tree().current_scene.add_child(ink_blob)
+	await animated_sprite_2d.animation_looped
+	animated_sprite_2d.play(&"idle")
+
+
+func _on_got_hit(body: Node2D) -> void:
+	if body is InkBlob and not body.can_hit_enemy:
+		return
+	body.queue_free()
+	animation_player.play(&"got hit")
+
+
+func start() -> void:
+	timer.timeout.connect(_on_timeout)
+	hit_box.body_entered.connect(_on_got_hit)
+	if odd_shoot:
+		await get_tree().create_timer(timer.wait_time / 2).timeout
+	timer.start()
+
+
+func remove() -> void:
+	timer.stop()
+	animation_player.play(&"remove")
+	await animation_player.animation_finished
+	queue_free()
