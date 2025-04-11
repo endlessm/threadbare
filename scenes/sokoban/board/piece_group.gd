eaa28c89708a2e93c43f984f99d34ada321577cc
Phoenix <68404504+PhoenixStroh@users.noreply.github.com>
2025-04-10 20:07:39 -0400

    Added copyright metatext on top of scripts.

--- scenes/sokoban/board/piece_group.gd
+++ scenes/sokoban/board/piece_group.gd
@@ -1,42 +1,55 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
 class_name PieceGroups
 extends Resource
 
 @export var groups: Array[StringName]
 
+
 static func is_valid(target_groups: PieceGroups) -> bool:
-    return target_groups and not target_groups.is_empty()
+	return target_groups and not target_groups.is_empty()
+
 
 static func from_group(target_group: StringName) -> PieceGroups:
-    return PieceGroups.new([target_group])
+	return PieceGroups.new([target_group])
+
 
 func _init(i_groups: Array[StringName] = []) -> void:
-    groups = i_groups
+	groups = i_groups
+
 
 func get_size() -> int:
-    return groups.size()
+	return groups.size()
+
 
 func is_empty() -> bool:
-    return groups.size() == 0
+	return groups.size() == 0
+
 
 func has_group(target_group: StringName) -> bool:
-    return groups.has(target_group)
+	return groups.has(target_group)
+
 
 func has_groups(target_groups: PieceGroups) -> bool:
-    if not target_groups: return false
+	if not target_groups:
+		return false
 
-    var result := true
-    for target_group in target_groups.groups:
-        if target_group not in groups:
-            result = false
-            break
-    
-    return result
+	var result := true
+	for target_group in target_groups.groups:
+		if target_group not in groups:
+			result = false
+			break
+
+	return result
+
 
 func append(group: StringName) -> void:
-    groups.append(group)
+	groups.append(group)
+
 
 func remove(group: StringName) -> void:
-    groups.erase(group)
+	groups.erase(group)
+
 
 func clear() -> void:
-    groups.clear()
+	groups.clear()
--- scenes/sokoban/rules/sokoban_rule.gd
+++ scenes/sokoban/rules/sokoban_rule.gd
@@ -1,29 +1,32 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
 class_name SokobanRule
 extends Resource
 
 enum CONTEXT {
-    NONE,
-    LATE,
+	NONE,
+	LATE,
 }
 
 enum DIRECTION {
-    UP,
-    RIGHT,
-    DOWN,
-    LEFT,
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT,
 }
 
 const DIRECTION_VECTORS := {
-    DIRECTION.UP: Vector2i.UP,
-    DIRECTION.RIGHT: Vector2i.RIGHT,
-    DIRECTION.DOWN: Vector2i.DOWN,
-    DIRECTION.LEFT: Vector2i.LEFT,
+	DIRECTION.UP: Vector2i.UP,
+	DIRECTION.RIGHT: Vector2i.RIGHT,
+	DIRECTION.DOWN: Vector2i.DOWN,
+	DIRECTION.LEFT: Vector2i.LEFT,
 }
 
-@export var direction : DIRECTION
-@export var contexts : Array[CONTEXT]
-@export var match_pattern : Array[RuleFragment]
-@export var replace_pattern : Array[RuleFragment]
+@export var direction: DIRECTION
+@export var contexts: Array[CONTEXT]
+@export var match_pattern: Array[RuleFragment]
+@export var replace_pattern: Array[RuleFragment]
+
 
 func get_vector() -> Vector2i:
-    return DIRECTION_VECTORS[direction]
+	return DIRECTION_VECTORS[direction]
