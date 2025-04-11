a7cdf0a02bcbda8f9b4d48ed0a8b4ad536138521
Phoenix <68404504+PhoenixStroh@users.noreply.github.com>
2025-04-10 20:07:38 -0400

    added basic features of sokoban

---
+++ scenes/sokoban/sokoban_rule.gd
@@ -0,0 +1,29 @@
+class_name SokobanRule
+extends Resource
+
+enum CONTEXT {
+    NONE,
+    LATE,
+}
+
+enum DIRECTION {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT,
+}
+
+const DIRECTION_VECTORS := {
+    DIRECTION.UP: Vector2i.UP,
+    DIRECTION.RIGHT: Vector2i.RIGHT,
+    DIRECTION.DOWN: Vector2i.DOWN,
+    DIRECTION.LEFT: Vector2i.LEFT,
+}
+
+@export var direction : DIRECTION
+@export var contexts : Array[CONTEXT]
+@export var match_pattern : Array[RuleFragment]
+@export var replace_pattern : Array[RuleFragment]
+
+func get_vector() -> Vector2i:
+    return DIRECTION_VECTORS[direction]
