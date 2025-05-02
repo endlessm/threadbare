endlessm/threadbare

commit bf96e5f721f24b39ef6c9b2fc13e7a9b7f56738e

Add wobble hint to music puzzle and fix continuous wobble issue

scenes/quests/lore_quests/quest_001/1_music_puzzle/components/music_puzzle.gd
@@ -17,6 +17,8 @@ signal solved
 ## If enabled, show messages in the console describing the player's progress (or not) in the puzzle
 @export var debug: bool = false
 
+var hint_timer: Timer = Timer.new()
+var _last_hint_rock: MusicalRock = null
 var _current_melody: int = 0
 var _position: int = 0
 
@@ -26,6 +28,10 @@ var _is_demo: bool = false
 func _ready() -> void:
 	if not Engine.is_editor_hint():
 		xylophone.note_played.connect(_on_note_played)
+	hint_timer.one_shot = true
+	hint_timer.wait_time = 6
+	hint_timer.timeout.connect(_on_hint_timer_timeout)
+	add_child(hint_timer)
 
 
 func _debug(fmt: String, args: Array = []) -> void:
@@ -43,18 +49,15 @@ func _on_note_played(note: String) -> void:
 		[melody, _position, melody[_position], note],
 	)
 	if melody[_position] != note:
-		if _position == 0:
-			_debug("Didn't match")
-			return
-
-		_debug("Matching again at start of melody...")
-		_position = 0
-
-	if melody[_position] != note:
 		_debug("Didn't match")
+		_position = 0
+		_debug("Matching again at start of melody...")
+		xylophone.stop_all_hints()
+		hint_timer.start()
 		return
 
 	_position += 1
+	hint_timer.start()
 	if _position != melody.length():
 		_debug("Played %s, awaiting %s", [melody.left(_position), melody.right(-_position)])
 		return
@@ -63,6 +66,8 @@ func _on_note_played(note: String) -> void:
 	fires[_current_melody].ignite()
 	_current_melody += 1
 	_position = 0
+
+	_clear_last_hint_rock()
 
 	if _current_melody == melodies.size():
 		_debug("All melodies played")
@@ -103,3 +108,39 @@ func play_demo_melody_of_fire(fire: BonfireSign) -> void:
 func play_demo_melody(melody: int) -> void:
 	for note in melodies[melody]:
 		await play_demo_note(note)
+
+
+func _on_hint_timer_timeout() -> void:
+	if _current_melody >= melodies.size():
+		return
+
+	var melody: String = melodies[_current_melody]
+	var expected_note := melody[_position]
+
+	if xylophone._notes.has(expected_note):
+		var rock = xylophone._notes[expected_note]
+		if rock != _last_hint_rock:
+			_clear_last_hint_rock()
+			_last_hint_rock = rock
+
+		if is_instance_valid(_last_hint_rock):
+			_last_hint_rock.wobble_silently()
+
+	hint_timer.start()
+
+
+func _clear_last_hint_rock() -> void:
+	if _last_hint_rock and is_instance_valid(_last_hint_rock):
+		_last_hint_rock.stop_hint()
+		_last_hint_rock = null
+
+
+func stop_hints() -> void:
+	hint_timer.stop()
+	_clear_last_hint_rock()
+
+
+func reset_hint_timer() -> void:
+	hint_timer.stop()
+	if _current_melody < melodies.size():
+		hint_timer.start()

scenes/quests/lore_quests/quest_001/1_music_puzzle/components/bard.gd
@@ -18,6 +18,11 @@ func _ready() -> void:
 			hint_levels[i] = 0
 
 
+func _on_dialogue_ended(_dialogue_resource: DialogueResource) -> void:
+	super._on_dialogue_ended(_dialogue_resource)
+	puzzle.reset_hint_timer()
+
+
 func play(note: String) -> void:
 	await puzzle.play_demo_note(note)
 

scenes/quests/lore_quests/quest_001/1_music_puzzle/components/dialogues/musician.dialogue
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
 ~ start
+do puzzle.stop_hints()
 if puzzle.is_solved():
 	=> item_hint
 

scenes/quests/lore_quests/quest_001/1_music_puzzle/components/musical_rock.gd
@@ -43,3 +43,14 @@ func play() -> void:
 	audio_stream_player_2d.play()
 	await audio_stream_player_2d.finished
 	animation_player.play(&"RESET")
+
+
+func wobble_silently() -> void:
+	animation_player.play("wobble")
+	await get_tree().create_timer(1.0).timeout
+	stop_hint()
+
+
+func stop_hint() -> void:
+	if animation_player.is_playing() and animation_player.current_animation == "wobble":
+		animation_player.play("RESET")

scenes/quests/lore_quests/quest_001/1_music_puzzle/components/musical_rock_xylophone.gd
@@ -20,3 +20,8 @@ func _ready() -> void:
 
 func play_note(note: String) -> void:
 	await _notes[note].play()
+
+
+func stop_all_hints() -> void:
+	for rock in _notes.values():
+		rock.stop_hint()

commit 09b04cd15ed92723e5e3baf54cbc6d57f428c2e5

Music Puzzle: On wrong note, match again at start of melody
Suppose that the sequence of notes that you have to play is C D E G, and you
play C D C D E G. This should be accepted as a valid solution, because although
the third note played is wrong for the 3rd note of the melody, it's the correct
first note. But this regressed in commit bf96e5f.

After failing to match the note in the middle of the melody, match it again at
the start.

Fixes https://github.com/endlessm/threadbare/issues/437

scenes/quests/lore_quests/quest_001/1_music_puzzle/components/music_puzzle.gd
@@ -71,10 +71,13 @@ func _on_note_played(note: String) -> void:
 		"Current melody %s position %d expecting %s, received %s",
 		[melody, _position, melody[_position], note],
 	)
-	if melody[_position] != note:
+	if _position != 0 and melody[_position] != note:
 		_debug("Didn't match")
 		_position = 0
 		_debug("Matching again at start of melody...")
+
+	if melody[_position] != note:
+		_debug("Didn't match")
 		xylophone.stop_all_hints()
 		if hint_levels.get(get_progress(), 0) >= wobble_hint_min_level:
 			hint_timer.start()