d1dbfd1f5d21ce72d36fe570bb0dd656ceb2a4e6
JacsonAulla <jacsonaulla@gmail.com>
2025-06-24 23:31:33 -0500

    ahora de verda esta el jeugo 3 >:c

--- scenes/quests/story_quests/ElUltimoBosque/2_ElUltimoBosque_combat/arribaAbajo.gd
+++ scenes/quests/story_quests/ElUltimoBosque/2_ElUltimoBosque_combat/arribaAbajo.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
 @export var speed: float = 100.0
-@export var top_limit: float = 176.0
-@export var bottom_limit: float = 858.0
+@export var limite_superior: float = 0
+@export var limite_inferior: float = 0
 
 var direction: int = 1  # 1 = baja, -1 = sube
 
 func _physics_process(_delta: float) -> void:
-	velocity.x = speed * direction
+	velocity.y = speed * direction
 	move_and_slide()
 
 	# Cambia de dirección si llegó al límite
-	if position.x >= bottom_limit:
+	if position.y >= limite_inferior:
 		direction = -1
-	elif position.x <= top_limit:
+	elif position.y <= limite_superior:
 		direction = 1
