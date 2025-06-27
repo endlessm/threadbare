6e826c1fb7d4c444c86817c0f2bdce554605fb8a
Will Thompson <wjt@endlessos.org>
2025-06-27 17:29:14 +0100

    Try using collision for the player touching the void

--- scenes/experimental/expanding_void/floor_eater.gd
+++ scenes/experimental/expanding_void/floor_eater.gd
@@ -2,21 +2,22 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Node
 
-const TERRAIN_SET: int = 0
-const VOID_TERRAIN: int = 8
-const NEIGHBORS := [
-	# TileSet.CELL_NEIGHBOR_BOTTOM_SIDE,
+@export var layers_to_destroy: Array[TileMapLayer]
+@export var directions_to_destroy: Array[TileSet.CellNeighbor] = [
+	TileSet.CELL_NEIGHBOR_BOTTOM_SIDE,
 	TileSet.CELL_NEIGHBOR_LEFT_SIDE,
 	TileSet.CELL_NEIGHBOR_TOP_SIDE,
-	# TileSet.CELL_NEIGHBOR_RIGHT_SIDE,
+	TileSet.CELL_NEIGHBOR_RIGHT_SIDE,
 ]
-
-@export var layers_to_destroy: Array[TileMapLayer]
 @export var void_layer: TileMapLayer
+@export var terrain_set_id: int = 0
+@export var void_terrain_name: String = "Void"
 @export var starting_point: Marker2D
 @export var player: Player
 
-@export_range(0.1, 2, 0.01, "suffix:seconds") var speed = 1.0
+@export_range(0.1, 2, 0.01, "suffix:seconds") var interval = 1.0
+
+var _void_terrain_index: int = -1
 
 var _timer: Timer
 
@@ -28,6 +29,15 @@ func _ready() -> void:
 	# Simplifies matching up tile coordinates; could be relaxed later
 	for layer in layers_to_destroy:
 		assert(layer.global_position == void_layer.global_position)
+		assert(layer.tile_set == void_layer.tile_set)
+
+	assert(void_layer.tile_set.get_terrain_sets_count() > terrain_set_id)
+	for index: int in range(0, void_layer.tile_set.get_terrains_count(terrain_set_id)):
+		var name: String = void_layer.tile_set.get_terrain_name(terrain_set_id, index)
+		if name == void_terrain_name:
+			_void_terrain_index = index
+			break
+	assert(_void_terrain_index >= 0)
 
 	_frontier = [void_layer.local_to_map(void_layer.to_local(starting_point.global_position))]
 
@@ -35,33 +45,40 @@ func _ready() -> void:
 	_timer.timeout.connect(_on_timer_timeout)
 	self.add_child(_timer)
 
-	_timer.start()
+	_timer.start(interval)
+
+
+func _physics_process(_delta: float) -> void:
+	if player.mode == Player.Mode.DEFEATED:
+		return
+
+	var n := player.get_slide_collision_count()
+	for i in range(n):
+		var collision := player.get_slide_collision(i)
+		if void_layer.has_body_rid(collision.get_collider_rid()):
+			player.mode = Player.Mode.DEFEATED
+			await get_tree().create_timer(2.0).timeout
+			SceneSwitcher.reload_with_transition(Transition.Effect.FADE, Transition.Effect.FADE)
 
 
 func _on_timer_timeout() -> void:
 	var new_frontier: Array[Vector2i]
-	var dead := false
 	for layer: TileMapLayer in layers_to_destroy:
-		layer.set_cells_terrain_connect(_frontier, TERRAIN_SET, -1)
-	void_layer.set_cells_terrain_connect(_frontier, TERRAIN_SET, VOID_TERRAIN)
+		layer.set_cells_terrain_connect(_frontier, terrain_set_id, -1)
+	void_layer.set_cells_terrain_connect(_frontier, terrain_set_id, _void_terrain_index)
 
 	for tile: Vector2i in _frontier:
-		var player_coords := void_layer.local_to_map(void_layer.to_local(player.global_position))
-		if tile == player_coords:
-			dead = true
-
-		for direction: TileSet.CellNeighbor in NEIGHBORS:
+		for direction: TileSet.CellNeighbor in directions_to_destroy:
 			var coord := void_layer.get_neighbor_cell(tile, direction)
+			if coord in new_frontier:
+				continue
+
 			for layer: TileMapLayer in layers_to_destroy:
 				var source_id := layer.get_cell_source_id(coord)
-				if source_id != -1 and coord not in new_frontier:
+				if source_id != -1:
 					new_frontier.append(coord)
+					break
 
 	_frontier = new_frontier
 	if not _frontier:
 		_timer.stop()
-
-	if dead:
-		player.mode = Player.Mode.DEFEATED
-		await get_tree().create_timer(2.0).timeout
-		SceneSwitcher.reload_with_transition(Transition.Effect.FADE, Transition.Effect.FADE)
