e7d26d36eea5ab9039572896eb48088aa301dcbf
franco310121 <franco310121@gmail.com>
2025-07-04 10:53:56 -0500

    animacion para target y enemigo

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,15 +3,21 @@ extends CharacterBody2D
 @onready var tiempo_peon_bala = $tiempo_bala_peon
 @onready var puntero_peon = $puntero_peon
 
-
+@onready var anim = $AnimatedSprite2D
 @export var vida_max = 10
 @export var vida = 10
 @export var bala_escena = preload("res://scenes/quests/story_quests/spacerage/pruebas/bala_enemiga_peon.tscn")
-@export var canIshoot = 0
+@export var canIshoot = false
+@export var isTarget = false
 
 func _ready():
 	tiempo_peon_bala.timeout.connect(_disparar)
 	tiempo_peon_bala.start()
+	if !isTarget:
+		anim.play("idle")
+	else:
+		anim.play("target_idle")
+	
 
 func _disparar():
 	
@@ -20,9 +26,10 @@ func _disparar():
 		print("Jugador no encontrado")
 		return
 	if jugador.global_position >= Vector2(2256.683, 368.451):
-		canIshoot = 1
+		canIshoot = true
 
-	if canIshoot == 1:
+	if canIshoot == true:
+		anim.play("alerted")
 		var bala = bala_escena.instantiate()
 		bala.global_position = puntero_peon.global_position
 		bala.direccion = (jugador.global_position - bala.global_position).normalized()
@@ -37,4 +44,15 @@ func recibir_daño(cantidad):
 
 	if vida <= 0:
 		tiempo_peon_bala.stop()
+		if !isTarget:
+			# animacion de muerte del enemigos
+			#anim.play("die")
+			print("entro 1")
+		else:
+			# animacion de muerte del target
+			print("entro 2")
+			anim.play("target_destroy")
+			await anim.animation_finished
+		
 		queue_free()
+		
