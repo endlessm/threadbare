68b58aa178f074db776f7f075662b95ae4579215
franco310121 <franco310121@gmail.com>
2025-07-04 03:49:38 -0500

    balas colisionan con las paredes

--- bala.gd
+++ bala.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-@export var velocidad = 500
-@export var daño = 3  
-var direccion = Vector2.ZERO
+@export var velocidad := 500
+@export var daño := 3
+@export var tilemap_paredes: TileMap  # TileMap asignado desde el editor
+
+var direccion := Vector2.ZERO
 
 @onready var detector = $detector
 
@@ -13,7 +15,14 @@ func _physics_process(delta):
 	if direccion != Vector2.ZERO:
 		position += direccion * velocidad * delta
 
+		# Verificar colisión con tilemap
+		if tilemap_paredes:
+			var celda = tilemap_paredes.local_to_map(global_position)
+			if tilemap_paredes.get_cell_source_id(0, celda) != -1:
+				print("Bala impactó contra pared en celda ", celda)
+				queue_free()
+
 func _on_body_entered(body):
 	if body.has_method("recibir_daño"):
 		body.recibir_daño(daño)
-		queue_free()
+	queue_free()
