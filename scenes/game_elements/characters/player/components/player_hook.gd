ddb474187764d2104ed03fc97cdce741b3920510
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-07-10 17:01:33 -0300

    WIP

--- scenes/game_elements/characters/player/components/player_hook.gd
+++ scenes/game_elements/characters/player/components/player_hook.gd
@@ -6,6 +6,8 @@ var hook_angle: float
 var hook_string: Line2D
 
 @onready var player: Player = self.owner as Player
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _ready() -> void:
@@ -13,6 +15,13 @@ func _ready() -> void:
 
 
 func add_string() -> void:
+	if not ray_cast_2d.is_colliding():
+		return
+	var hookable = ray_cast_2d.get_collider() as HookableArea
+	if not hookable:
+		return
+	var p = hookable.get_hooking_point()
+
 	if hook_string:
 		hook_string.queue_free()
 	hook_string = Line2D.new()
@@ -21,7 +30,8 @@ func add_string() -> void:
 	hook_string.texture_mode = Line2D.LINE_TEXTURE_TILE
 	hook_string.joint_mode = Line2D.LINE_JOINT_ROUND
 	hook_string.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
-	hook_string.add_point(Vector2(300, 0).rotated(rotation))
+	# hook_string.add_point(Vector2(300, 0).rotated(rotation))
+	hook_string.add_point(p - player.global_position - position)
 	hook_string.add_point(Vector2.ZERO)
 	# hook_string.z_index = player.z_index - 1
 	player.add_sibling(hook_string)
@@ -44,5 +54,10 @@ func _unhandled_input(_event: InputEvent) -> void:
 
 func _process(_delta: float) -> void:
 	rotation = hook_angle
+	if ray_cast_2d.is_colliding():
+		sprite_2d.modulate = Color.WHITE
+	else:
+		sprite_2d.modulate = Color(Color.WHITE, 0.2)
+
 	if hook_string:
 		hook_string.points[-1] = player.position + position - hook_string.position
