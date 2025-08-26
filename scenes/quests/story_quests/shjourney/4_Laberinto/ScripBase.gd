d83ccc5ae80bf26bc1736170ca9466e7503442d7
xXLeopoldoXx <britoleopoldo02@gmail.com>
2025-08-26 15:35:44 -0500

    ADDING key collection mechanic logic outside of player.gd

--- scenes/quests/story_quests/shjourney/4_Laberinto/ScripBase.gd
+++ scenes/quests/story_quests/shjourney/4_Laberinto/ScripBase.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+var llaves: int = 0
+@export var llaves_maximas: int = 3
+@onready var puerta: StaticBody2D = $Puerta
+
+func ActualizarLlaves():
+	if llaves >= llaves_maximas:
+		puerta.queue_free()
+
 
 func _on_timer_puerta_timeout() -> void:
 	$"CamaraPuerta".enabled = false
