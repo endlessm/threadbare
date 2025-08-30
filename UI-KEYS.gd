435be6af2306f1711dc91a43236152daa66f2ba3
xXLeopoldoXx <britoleopoldo02@gmail.com>
2025-08-29 23:19:45 -0500

    Adding a HDU for Key collection

---
+++ UI-KEYS.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+var llaves = 0
+
+func _ready():
+	
+	$KeysCollectText.text = "0/3"
+
+func handleKeyCollector(llaves_actuales: int, llaves_maximas: int):
+	print("Key Collected - UI Updated")
+	llaves = llaves_actuales
+	$KeysCollectText.text = str(llaves_actuales) + "/" + str(llaves_maximas)
