d3d5f0526d79b694852e08cca2d608e6d3f6d536
Pierina Nicole MB <pierina.mb4@gmail.com>
2025-09-04 15:57:09 -0500

    Update input_key.gd

--- scenes/game_elements/props/hint/input_key/input_key.gd
+++ scenes/game_elements/props/hint/input_key/input_key.gd
@@ -4,6 +4,11 @@ extends TextureRect
 
 @export var action_name: StringName
 
+@export var keyboard_texture: Texture2D
+@export var xbox_controller_texture: Texture2D
+@export var playstation_controller_texture: Texture2D
+@export var nintendo_controller_texture: Texture2D
+@export var steam_controller_texture: Texture2D
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed(action_name):
@@ -16,5 +21,22 @@ func _ready() -> void:
 
 
 func _on_input_device_changed(device: String, device_index: int) -> void:
-	print("XBox? ", device == InputHelper.DEVICE_XBOX_CONTROLLER)
+	print("Device detected? ", device == InputHelper.DEVICE_XBOX_CONTROLLER)
 	print("Device index? ", device_index) # Probably 0
+
+	match device:
+		InputHelper.DEVICE_KEYBOARD:
+			if keyboard_texture:
+				texture = keyboard_texture
+		InputHelper.DEVICE_XBOX_CONTROLLER:
+			if xbox_controller_texture:
+				texture = xbox_controller_texture
+		InputHelper.DEVICE_PLAYSTATION_CONTROLLER:
+			if playstation_controller_texture:
+				texture = playstation_controller_texture
+		InputHelper.DEVICE_NINTENDO_CONTROLLER:
+			if nintendo_controller_texture:
+				texture = nintendo_controller_texture
+		InputHelper.DEVICE_STEAMDECK_CONTROLLER:
+			if steam_controller_texture:
+				texture = steam_controller_texture
