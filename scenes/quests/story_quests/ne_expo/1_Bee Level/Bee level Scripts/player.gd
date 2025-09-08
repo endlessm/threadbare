109ce4897e2d7ae9a16e3c78688f0034974598c2
Ben McVety <benmcvety@gmail.com>
2025-09-08 16:48:45 -0400

    Created Attacks, Explosions, knockback
    Added scenes for icicle spear attack, explosion. Added explosion assets for different attacks, sound effects for firing icicle_spear, enemies getting hit, and enemy explosion 1.

--- scenes/quests/story_quests/ne_expo/1_Bee Level/Bee level Scripts/player.gd
+++ scenes/quests/story_quests/ne_expo/1_Bee Level/Bee level Scripts/player.gd
@@ -1,11 +1,33 @@
 extends CharacterBody2D
 
 var hp = 80
+
+#Attacks
+var iceSpear = preload("res://scenes/quests/story_quests/ne_expo/1_Bee Level/Bee Level Scenes/Attack/ice_spear.tscn")
+
+#AttackNodes
+@onready var iceSpearTimer = get_node("%IceSpearTimer")
+@onready var iceSpearAttackTimer = get_node("%IceSpearATtackTimer")
+
+#IceSpear
+var icespear_ammo = 0
+var icespear_baseammo = 1
+var icespear_attackspeed = 1.5
+var icespear_level = 1
+
+#Enemy Related
+var enemy_close = []
+
+
 @export var movement_speed = 250.0
 @onready var sprite = $"Bee (player)"
 @onready var spriteShadow = $"Bee (player)/Shadow (player)"
 
-
+func _ready():
+	attack()
+	
+	
+	
 func _physics_process(delta: float) -> void:
 	movement()
 	
@@ -22,8 +44,47 @@ func movement():
 	velocity = mov.normalized()*movement_speed
 	move_and_slide()
 	
+func attack():
+	if icespear_level > 0:
+		iceSpearTimer.wait_time = icespear_attackspeed
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
 
-
-func _on_hurt_box_hurt(damage: Variant) -> void:
+func _on_hurt_box_hurt(damage: Variant, _angle, _knockback) -> void:
 	hp -= damage
 	print(hp)
+
+
+func _on_ice_spear_timer_timeout() -> void:
+	icespear_ammo += icespear_baseammo
+	iceSpearAttackTimer.start()
+
+
+func _on_ice_spear_a_ttack_timer_timeout() -> void:
+	if icespear_ammo > 0:
+		var icespear_attack = iceSpear.instantiate()
+		icespear_attack.position = position
+		icespear_attack.target = get_random_target()
+		icespear_attack.level = icespear_level
+		add_child(icespear_attack)
+		icespear_ammo -= 1
+		if icespear_ammo > 0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+		
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
+	if enemy_close.has(body):
+		enemy_close.erase(body)
