a458b4ddf549008e8f0888de05088c09f9fbcaba
Jeesson Yman <u23326041@utp.com>
2025-11-05 17:16:12 -0500

    minigame preview 2

--- scenes/quests/story_quests/estation _echo_9/2_combat/combat_components/enemy_data.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name EnemyData
-
-@export var speed: float = 45.0
-@export var patrol_distance: float = 200.0
-@export var vision_range: float = 150.0
-@export var alert_duration: float = 2.0
