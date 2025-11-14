6d12e8cf177c261c4a9d63c8452d9dab02fd7b18
Jeesson Yman <u23326041@utp.com>
2025-11-14 01:54:36 -0500

    se actualizo el minijuego 2

--- scenes/quests/story_quests/estation _echo_9/2_containment_passages/containment_passages_enemies/eco_sentinel.gd
+++
@@ -1,8 +0,0 @@
-# eco_sentinel.gd
-extends "res://scenes/quests/story_quests/estation _echo_9/2_containment_passages/containment_passages_enemies/eco_base.gd"
-
-func _ready() -> void:
-	$Timer.timeout.connect(_on_timer_timeout)
-
-func _on_timer_timeout() -> void:
-	rotate(deg_to_rad(90))
--- scenes/quests/story_quests/estation _echo_9/2_containment_passages/containment_passages_player/player.gd
+++
@@ -1,9 +0,0 @@
-# player.gd
-extends CharacterBody2D
-
-@export var speed: float = 200.0
-
-func _physics_process(delta: float) -> void:
-	var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * speed
-	move_and_slide()
