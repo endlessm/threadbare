942cd4f6507af1fa0caa256f3d945f17ede34734
kphero <k_homen@hotmail.com>
2025-11-21 16:50:01 -0500

    reformatted with gdformat

--- scenes/globals/audio/audio_manager.gd
+++ scenes/globals/audio/audio_manager.gd
@@ -7,11 +7,14 @@ extends Node
 @onready var ui_click_back_sound: AudioStreamPlayer = %ClickBack
 @onready var ui_click_toggle_sound: AudioStreamPlayer = %ClickToggle
 
+
 func play_ui_click():
 	ui_click_sound.play()
 
+
 func play_ui_click_back():
 	ui_click_back_sound.play()
-	
+
+
 func play_ui_click_toggle():
 	ui_click_toggle_sound.play()
