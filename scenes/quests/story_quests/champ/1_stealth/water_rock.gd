51f3ffa83c44619b1257ce64ea7d1608123bf0ef
shade <80239912+shadeydo@users.noreply.github.com>
2026-02-05 17:00:22 -0600

    Visual improvements to stealth’s water (#1889)
    - Rocks along causeway to show movement more clearly
    - Ripples in water to be more consistent with combat mini-game

---
+++ scenes/quests/story_quests/champ/1_stealth/water_rock.gd
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+		# randomize starting frame for visual variance
+		var rng = RandomNumberGenerator.new()
+		rng.randomize()
+		frame = rng.randi_range(0,7)
+		
+		play("water_rock")
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
