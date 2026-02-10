6a7780be16c53ea6a08ed09a5c6bf3f3dc2de3db
shade <80239912+shadeydo@users.noreply.github.com>
2026-02-10 15:57:55 -0500

    Made water decorations scene-based
    This addresses the comments in the stealth minigame PR a couple days ago.
    
    - Made new WaterRock scene that is instantiated in the main stage instead of duplicate instances
    - Did the same for the water ripples to make it easier to make changes
    - Added RandomFrameSpriteBehavior node to both animations instead of a custom randomization script for each (I still had to have a script to start the animation playing though, can't figure out how to get around this.)

---
+++ scenes/quests/story_quests/champ/1_stealth/stealth_components/lake decorations/water ripples/water.gd
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _enter_tree() -> void:
+	play("1")
