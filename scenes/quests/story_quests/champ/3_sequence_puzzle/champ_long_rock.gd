2a0ce0b86e8789bb9cae977a75eff526be4276ea
felixwalberg <122999576+felixwalberg@users.noreply.github.com>
2026-02-10 06:31:06 -0500

    Champ learner assets (#1851)
    This addresses some of the additions for learner contributions outlined in the Champy GDD:
    
    - BackgroundMusic nodes in each scene for learners to add background music
    - Renaming of nodes to match instructions for changing/inserting art pngs
    - Graying out obstacles in combat level to imply the need for learner contributions
    - Replacing audio file for sequence puzzle rock object
    
    ---------
    
    Co-authored-by: Will Thompson <wjt@endlessaccess.org>

--- scenes/quests/story_quests/champ/3_sequence_puzzle/champ_long_rock.gd
+++ scenes/quests/story_quests/champ/3_sequence_puzzle/champ_long_rock.gd
@@ -3,6 +3,7 @@
 extends Node2D
 @onready var rock: AnimatedSprite2D = $AnimatedSprite2D
 @onready var area_2d: Area2D = $AnimatedSprite2D/Area2D
+@onready var audio_player: AudioStreamPlayer2D = $"Audio Player"
 
 ## Boolean value representing if the rock is under water
 @export var submerged: bool = true:
@@ -28,10 +29,13 @@ func _ready() -> void:
 
 ## Function to tell champ sequence puzzle script the player guessed wrong and must be moved back
 func _on_area_2d_body_entered(_body: Node2D) -> void:
+	audio_player.play()
 	await get_tree().create_timer(1.5).timeout
 	water_entered.emit()
 
 
 ## Toggles the water level of the rock, updating the animation and knocking the player back if necessary.
 func toggle_water_level() -> void:
+	if not submerged:
+		await get_tree().create_timer(0.5).timeout
 	submerged = not submerged
