0d4cd1f108f71f8b0717e9c84804f81f91189c7c
shade <80239912+shadeydo@users.noreply.github.com>
2026-02-11 12:38:29 -0500

    Switched to using the default water_rock instead of custom
    Addressed the new comments in the #1901 PR.
    I didn't realize that there was already a water_rock.tcsn file shared with all story quests, so I switched the stealth mini game to using that!
    Also, turned on autoplay for the water ripples instead of using a custom script to do so.

--- scenes/quests/story_quests/champ/1_stealth/stealth_components/lake decorations/water ripples/water.gd
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: The Threadbare Authors
-# SPDX-License-Identifier: MPL-2.0
-extends AnimatedSprite2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _enter_tree() -> void:
-	play("1")
--- scenes/quests/story_quests/champ/1_stealth/stealth_components/lake decorations/water rock/water_rock.gd
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: The Threadbare Authors
-# SPDX-License-Identifier: MPL-2.0
-extends AnimatedSprite2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-		play()
-	
