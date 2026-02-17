5ed7f5c5c0c8548d8e35fd15921783af9e9316a4
Manuel Quiñones <manuq@endlessaccess.org>
2026-02-17 14:06:26 -0300

    Fixup: Persist random seed

--- scenes/game_elements/fx/clouds_shadow/components/clouds_shadow.gd
+++ scenes/game_elements/fx/clouds_shadow/components/clouds_shadow.gd
@@ -5,17 +5,27 @@ extends Parallax2D
 
 @export_tool_button("Random Clouds") var randomize_button: Callable = randomize
 
+@export var _seed: int:
+	set = _set_seed
+
 var texture: NoiseTexture2D
 var noise: FastNoiseLite
 
 @onready var color_rect: ColorRect = %ColorRect
 
 
+func _set_seed(new_seed: int) -> void:
+	_seed = new_seed
+	if noise:
+		noise.seed = _seed
+		await texture.changed
+
+
 func _ready() -> void:
 	texture = (color_rect.material as ShaderMaterial).get_shader_parameter("shadow_texture")
 	noise = texture.noise
+	_set_seed(_seed)
 
 
 func randomize() -> void:
-	noise.seed = randi()
-	await texture.changed
+	await _set_seed(randi())
