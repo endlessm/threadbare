00decc0d3fb1b163acbfd3d45c25585bd5f8f624
Will Thompson <wjt@endlessaccess.org>
2026-02-18 10:07:11 +0000

    Only fade background music when changing streams
    Previously, the background music always faded out during the scene-exit
    transition, then faded in during the scene-enter transition. This had:
    two negative consequences:
    
    1. It sounds bad when the successive scenes have the same music (e.g.
       the three waves of ink combat);
    2. It means the opening chord of each new piece of music is guaranteed
       to be inaudible since the 1-second fade-in is still ongoing.
    
    Instead:
    
    - Do not fade the music when the scene fades out;
    - When the new scene arrives, with a BackgroundMusic node:
      - If the stream is the same, do nothing (fixing point 1 above);
      - Else, fade out the old stream for 1 second, then start playing the
        new stream at full volume (fixing point 2 above).
    
    To do this, move most of the logic from BackgroundMusic to MusicPlayer.
    Remove the ability to pause/resume BackgroundMusic in the editor for
    simplicity, leaving only Play/Stop.
    
    Improve documentation of the two cooperating scripts.
    
    Resolves https://github.com/endlessm/threadbare/issues/637

--- scenes/game_elements/props/background_music/components/background_music.gd
+++ scenes/game_elements/props/background_music/components/background_music.gd
@@ -1,17 +1,22 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
 @tool
+class_name BackgroundMusic
 extends Node
+## Specify the background music to play in a scene.
+##
+## This controls the singleton MusicPlayer node, which outlives every scene, so
+## that music can keep playing between scenes.
+##
+## There should be at most one BackgroundMusic component in a scene.
 
-@export_tool_button("Play") var play_button: Callable = _play
-@export_tool_button("Stop") var stop_button: Callable = _stop
-@export_tool_button("Pause/Resume") var pause_resume_button: Callable = _pause_resume
+@export_tool_button("Play") var play_button: Callable = play
+@export_tool_button("Stop") var stop_button: Callable = stop
+
 @export var stream: AudioStream:
 	set(new_value):
 		stream = new_value
 		update_configuration_warnings()
-
-@onready var audio_stream_player: AudioStreamPlayer = MusicPlayer.background_music_player
 
 
 func _get_configuration_warnings() -> PackedStringArray:
@@ -22,63 +27,29 @@ func _get_configuration_warnings() -> PackedStringArray:
 
 
 func _ready() -> void:
-	if Engine.is_editor_hint():
-		return
-
-	if stream:
-		_fade_in()
-		_play()
-	else:
-		_stop()
-
-	Transitions.started.connect(self._fade_out)
-
-
-func _play() -> void:
-	if audio_stream_player.stream == stream and audio_stream_player.playing:
-		return
-
-	if stream:
-		audio_stream_player.stream = stream
-		audio_stream_player.play()
-
-
-func _stop() -> void:
-	audio_stream_player.stop()
-
-
-func _pause_resume() -> void:
-	if audio_stream_player.is_playing():
-		audio_stream_player.stream_paused = true
-	else:
-		audio_stream_player.stream_paused = false
-
-
-func _fade_in() -> void:
-	create_tween().tween_property(audio_stream_player, "volume_linear", 1.0, 1.0)
-
-
-func _fade_out() -> void:
-	create_tween().tween_property(audio_stream_player, "volume_linear", 0.0, 1.0)
+	if not Engine.is_editor_hint():
+		play()
 
 
 func _exit_tree() -> void:
-	## Without this, if you click the Play button and then close the scene
-	## in the editor, the music will keep playing.
 	if Engine.is_editor_hint():
-		_stop()
+		stop()
+	else:
+		MusicPlayer.scene_about_to_change()
 
 
-## If [member stream] is an [AudioStreamInteractive], and music is playing,
+## Start playing [member stream], if it is not already playing. Does nothing if
+## [member stream] is already playing.
+func play() -> void:
+	MusicPlayer.play_stream(stream)
+
+
+## Stop playing background music.
+func stop() -> void:
+	MusicPlayer.play_stream(null)
+
+
+## If [member stream] is an [AudioStreamInteractive], and is playing,
 ## switch to [param clip_name].
 func switch_to_clip(clip_name: StringName) -> void:
-	if not audio_stream_player.is_playing():
-		push_warning("Not currently playing")
-		return
-
-	if audio_stream_player.stream is not AudioStreamInteractive:
-		push_warning("Switching clips is only supported with AudioStreamInteractive")
-		return
-
-	var playback := audio_stream_player.get_stream_playback() as AudioStreamPlaybackInteractive
-	playback.switch_to_clip_by_name(clip_name)
+	MusicPlayer.switch_to_clip(clip_name)
