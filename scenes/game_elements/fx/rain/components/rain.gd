459e187333dda13f17efedb0583939993a2375d2
Manuel Quiñones <manuq@endlessaccess.org>
2026-02-23 15:33:44 -0300

    Add rain overlay effect

---
+++ scenes/game_elements/fx/rain/components/rain.gd
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends CanvasLayer
+
+@onready var gpu_particles_2d: GPUParticles2D = %GPUParticles2D
+
+
+func randomize() -> void:
+	var rain_type := randf()
+	if rain_type < 0.25:
+		# 25% chances of having a drizzle.
+		gpu_particles_2d.amount_ratio = randf_range(0, 0.1)
+	elif rain_type >= 0.75:
+		# 25% chances of having a steady rain.
+		gpu_particles_2d.amount_ratio = randf_range(0.8, 1)
+	else:
+		# 50% chances of having something in between.
+		gpu_particles_2d.amount_ratio = randf_range(0.1, 0.8)
