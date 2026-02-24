89e346c06a9723adbfc3b8d3893ade0b315cd73d
Manuel Quiñones <manuq@endlessaccess.org>
2026-02-24 09:14:41 -0300

    Add TimeAndWeather node

--- scenes/game_elements/fx/clouds_shadow/components/clouds_shadow.gd
+++ scenes/game_elements/fx/clouds_shadow/components/clouds_shadow.gd
@@ -10,6 +10,7 @@ extends Parallax2D
 
 var texture: NoiseTexture2D
 var noise: FastNoiseLite
+var tween: Tween
 
 @onready var color_rect: ColorRect = %ColorRect
 
@@ -29,3 +30,22 @@ func _ready() -> void:
 
 func randomize() -> void:
 	await _set_seed(randi())
+
+
+func fade_in(duration: float = 1) -> void:
+	if tween:
+		tween.kill()
+	color_rect.modulate = Color.BLACK
+	visible = true
+	tween = create_tween()
+	tween.tween_property(color_rect, "modulate", Color.WHITE, duration)
+	await tween.finished
+
+
+func fade_out(duration: float = 1) -> void:
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(color_rect, "modulate", Color.BLACK, duration)
+	await tween.finished
+	visible = false
