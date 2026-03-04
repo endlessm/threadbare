b8d26704d2775dab70bcd7c2210c1545893d4afa
Will Thompson <will@willthompson.co.uk>
2026-03-04 18:18:23 +0000

    WIP: Overworld prototype
    Connect Fray's End, the Musician's lair, and void runner bidirectionally
    to the overworld.
    
    Abandon any running quest when returning to the overworld.
    
    Gate access to the temple/void_runner teleporter with a blocking area of
    void, removed when you complete the musician quest.
    
    Enable grappling only once you complete the void quest. I'll later put a
    teleporter to the third quest onto the disconnected island that you have
    to grapple to.
    
    TODO: remove the LoreQuest Elder from Fray's End.
    
    TODO: take the player back to the Overworld from the last round of the
    respective quests.
    
    TODO: take the player back to the Overworld when they abandon quest.

---
+++ overworld.gd
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Node2D
+
+
+@onready var player: Player = %Player
+
+
+func _ready() -> void:
+	GameState.abandon_quest()
+
+
+func _on_void_quest_unlocker_toggled(satisfied: bool) -> void:
+	if satisfied:
+		player.mode = Player.Mode.HOOKING
+	else:
+		player.mode = Player.Mode.COZY
