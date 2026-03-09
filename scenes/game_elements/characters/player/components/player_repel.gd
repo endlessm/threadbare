83803851a5f45dff8741390df5927a991c794a3d
Manuel Quiñones <manuq@endlessaccess.org>
2026-03-09 16:18:39 -0300

    Player: Extract "got hit" from PlayerRepel
    Into a new PlayerHarm node inside the Player scene. There is no actual harm today, but the "got hit" animation and handling of the
    projectiles entering the player hitbox are here now, split from the PlayerRepel ability.
    
    Also, connect the signals from the editor, not in the _ready() function.

--- scenes/game_elements/characters/player/components/player_repel.gd
+++ scenes/game_elements/characters/player/components/player_repel.gd
@@ -10,16 +10,9 @@ signal repelling_changed(repelling: bool)
 var repelling: bool = false:
 	set = _set_repelling
 
-@onready var hit_box: Area2D = %HitBox
-@onready var got_hit_animation: AnimationPlayer = %GotHitAnimation
 @onready var air_stream: Area2D = %AirStream
 
 @onready var player: Player = self.owner as Player
-
-
-func _ready() -> void:
-	hit_box.body_entered.connect(_on_body_entered)
-	air_stream.body_entered.connect(_on_air_stream_body_entered)
 
 
 func _set_repelling(new_repelling: bool) -> void:
@@ -34,22 +27,5 @@ func _unhandled_input(_event: InputEvent) -> void:
 		repelling = false
 
 
-func _on_body_entered(body: Node2D) -> void:
-	body = body as Projectile
-	if not body:
-		return
-	body.add_small_fx()
-	body.queue_free()
-	got_hit_animation.play(&"got_hit")
-	CameraShake.shake()
-
-
 func _on_air_stream_body_entered(body: Projectile) -> void:
 	body.got_hit(owner)
-
-
-func _notification(what: int) -> void:
-	match what:
-		NOTIFICATION_DISABLED:
-			got_hit_animation.play(&"RESET")
-			got_hit_animation.advance(0)
