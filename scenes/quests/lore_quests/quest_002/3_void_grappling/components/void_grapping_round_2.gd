563568656cc2ae8298a6097de457028e308f40af
Manuel Quiñones <manuq@endlessaccess.org>
2026-03-10 23:50:10 -0300

    Lore quests: grant abilities
    - Grant "repel" at the beginning of ink_combat_round_1
    - Grant "grapple" at the beginning of grappling_hook_start
    - Grant "longer thread" when collecting the powerup in grappling_hook_powerup
    - Grant "longer thread" when collecting the powerup in void_grappling_round_2 (although it should be already given at this point)
    - Remove hacky way to set longer thread

--- scenes/quests/lore_quests/quest_002/3_void_grappling/components/void_grapping_round_2.gd
+++ scenes/quests/lore_quests/quest_002/3_void_grappling/components/void_grapping_round_2.gd
@@ -2,21 +2,12 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
 
-const LongerHook = preload(
-	"res://scenes/quests/lore_quests/quest_002/2_grappling_hook/components/longer_hook.gd"
-)
-
 @onready var player: Player = %Player
-@onready var frame_camera_behavior: FrameCameraBehavior = %FrameCameraBehavior
 @onready var void_chasing: CharacterBody2D = %VoidChasing
 
 
-func _ready() -> void:
-	frame_camera_behavior.frame_target = player.get_node("PlayerHook/HookEnding")
-
-
 func _on_button_item_collected() -> void:
-	LongerHook.grant_longer_hook(player)
+	GameState.set_ability(Enums.PlayerAbilities.ABILITY_B_MODIFIER_1, true)
 
 	# Zoom out the camera when collecting the powerup, because now the player
 	# can throw a longer thread:
