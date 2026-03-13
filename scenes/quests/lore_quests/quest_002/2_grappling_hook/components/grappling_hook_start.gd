95b1a79ec70ce549a550b2166b8fef66771f19f6
Manuel Quiñones <manuq@endlessaccess.org>
2026-03-13 10:06:57 -0300

    Void grappling hook levels: Remove duplicated grappling_hook_end scene
    And reuse grappling_hook_start instead. Add a teleport to the corresponding place
    and listen to its player_teleported signal to disable the initial cinematic,
    and play the "floor eating" animation directly (without dialogue) instead.
    
    Change the path of the Void eating animation to play in reverse, and add one more
    point to the patrol path. This looks better and leaves the entity off-screen.
    
    Caveat: there is no way to skip the animation when returning to this scene, because
    what the animation actually does is enable the Void enemy which in turn paints the
    tiles in it's process.

--- scenes/quests/lore_quests/quest_002/2_grappling_hook/components/grappling_hook_start.gd
+++ scenes/quests/lore_quests/quest_002/2_grappling_hook/components/grappling_hook_start.gd
@@ -2,6 +2,27 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
 
+var _play_cinematic: bool = true
+
+@onready var cinematic: Cinematic = $Cinematic
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var void_patrolling: CharacterBody2D = %VoidPatrolling
+
 
 func _ready() -> void:
-	GameState.set_ability(Enums.PlayerAbilities.ABILITY_B, true)
+	## Delay processing
+	#await get_tree().create_timer(1.0).timeout
+	#area_2d_from_start.process_mode = Node.PROCESS_MODE_INHERIT
+	if _play_cinematic:
+		cinematic.start()
+
+
+func _on_spawn_point_from_powerup_player_teleported() -> void:
+	# The player is coming back from the east, so play the
+	# animation directly. This is actually letting the Void
+	# add tiles.
+	_play_cinematic = false
+	if not is_node_ready():
+		await ready
+	void_patrolling.visible = false
+	animation_player.play(&"eat_floor")
