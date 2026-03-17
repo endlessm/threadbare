0837642ca0e10ac27bfbac91c36915fe2f4906ae
Manuel Quiñones <manuq@endlessaccess.org>
2026-03-17 16:57:26 -0300

    after_the_tremor StoryQuest: Fix combat scene
    Use the new action input export of PlayerRepel to change it.
    Disable the ability after the boss is defeated with the PlayerMode
    node.
    
    Also remove moving the PlayerInteraction inner node to a far away
    position, and then back after the boss is defeated. There doesn't
    seem to be a need for this.

--- scenes/quests/story_quests/after_the_tremor/3_combat/scripts/combat.gd
+++ scenes/quests/story_quests/after_the_tremor/3_combat/scripts/combat.gd
@@ -17,6 +17,8 @@ extends Node2D
 @onready var boss_info = $UI/BossInfo
 @onready var health_bar = $UI/BossInfo/BossBar 
 
+@onready var player_mode: PlayerMode = %PlayerMode
+
 func _ready():
 	if collectible:
 		collectible.revealed = false
@@ -33,18 +35,13 @@ func _ready():
 			health_bar.value = boss_health.needed_amount
 	
 	if boss: boss.start()
-	if charlie: _setup_combat_player(charlie, "move_left")
-	if bryan: _setup_combat_player(bryan, "move_right")
+	if charlie: _setup_combat_player(charlie)
+	if bryan: _setup_combat_player(bryan)
 	if music and not music.playing: music.play()
 
-func _setup_combat_player(p_node, action_name):
+func _setup_combat_player(p_node):
 	p_node.walk_speed = 0.0
 	p_node.run_speed = 0.0
-	var fighting_comp = p_node.get_node_or_null("PlayerFighting")
-	if fighting_comp: fighting_comp.input_action = action_name
-	p_node.mode = p_node.Mode.FIGHTING
-	var interaction = p_node.get_node_or_null("PlayerInteraction")
-	if interaction: interaction.position = Vector2(99999, 99999)
 
 
 func _update_health_bar(current_damage_taken):
@@ -60,13 +57,11 @@ func _on_boss_defeated():
 	if tile_grass: tile_grass.modulate = Color.WHITE
 	if tile_stones: tile_stones.modulate = Color.WHITE
 	
+	player_mode.mode = PlayerMode.Mode.COZY
+
 	if boss_info:
 		boss_info.visible = false
 	if charlie:
-		var fighting = charlie.get_node_or_null("PlayerFighting")
-		if fighting:
-			fighting.set_process_unhandled_input(false)
-			fighting.is_fighting = false 
 		var sprite = charlie.get_node_or_null("PlayerSprite") 
 		if not sprite: sprite = charlie.find_child("AnimatedSprite2D", true, false)
 		if sprite: sprite.play("idle"); sprite.stop()
@@ -74,8 +69,3 @@ func _on_boss_defeated():
 	if bryan:
 		bryan.walk_speed = 300.0
 		bryan.run_speed = 500.0
-		bryan.mode = bryan.Mode.COZY
-		var fighting = bryan.get_node_or_null("PlayerFighting")
-		if fighting: fighting.set_process_unhandled_input(false); fighting.is_fighting = false
-		var interaction = bryan.get_node_or_null("PlayerInteraction")
-		if interaction: interaction.position = Vector2.ZERO
