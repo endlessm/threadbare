b7d88cb7c2686d586cc9e8dfcfb73d14dfe4a464
Manuel Quiñones <manuq@endlessaccess.org>
2026-03-27 13:29:16 -0300

    fixup! void_grappling_round_2: Use powerup

--- scenes/quests/lore_quests/quest_002/2_grappling_hook/components/grappling_hook_powerup.gd
+++ scenes/quests/lore_quests/quest_002/2_grappling_hook/components/grappling_hook_powerup.gd
@@ -2,10 +2,21 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
 
+var zoom_tween: Tween
 
-func _on_longer_thread_powerup_collected() -> void:
-	# Zoom out the camera when collecting the powerup, because now the player
-	# can throw a longer thread:
+
+func _ready() -> void:
+	GameState.abilities_changed.connect(_on_abilities_changed)
+	_on_abilities_changed()
+
+
+func _on_abilities_changed() -> void:
+	var has_longer_thread := GameState.has_ability(Enums.PlayerAbilities.ABILITY_B_MODIFIER_1)
+	var camera_zoom := Vector2(0.5, 0.5) if has_longer_thread else Vector2(1.0, 1.0)
+	# Zoom out when the player can throw a longer thread:
 	var camera: Camera2D = get_viewport().get_camera_2d()
-	var zoom_tween := create_tween()
-	zoom_tween.tween_property(camera, "zoom", Vector2(0.8, 0.8), 1.0)
+	if camera.zoom != camera_zoom:
+		if zoom_tween:
+			zoom_tween.kill()
+		zoom_tween = create_tween()
+		zoom_tween.tween_property(camera, "zoom", camera_zoom, 1.0)
--- scenes/quests/lore_quests/quest_002/3_void_grappling/components/void_grapping_round_2.gd
+++ scenes/quests/lore_quests/quest_002/3_void_grappling/components/void_grapping_round_2.gd
@@ -2,14 +2,31 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
 
+var zoom_tween: Tween
+
 @onready var player: Player = %Player
 @onready var void_chasing: CharacterBody2D = %VoidChasing
 
 
-func _on_longer_thread_powerup_collected() -> void:
-	# Zoom out the camera when collecting the powerup, because now the player
-	# can throw a longer thread:
+func _ready() -> void:
+	GameState.abilities_changed.connect(_on_abilities_changed)
+
+	# TODO: This level removes the longer thread modifier when started.
+	# Otherwise the Void chase can't be triggered.
+	GameState.set_ability(Enums.PlayerAbilities.ABILITY_B_MODIFIER_1, false)
+
+
+func _on_abilities_changed() -> void:
+	var has_longer_thread := GameState.has_ability(Enums.PlayerAbilities.ABILITY_B_MODIFIER_1)
+	var camera_zoom := Vector2(0.5, 0.5) if has_longer_thread else Vector2(1.0, 1.0)
+	# Zoom out when the player can throw a longer thread:
 	var camera: Camera2D = get_viewport().get_camera_2d()
-	var zoom_tween := create_tween()
-	zoom_tween.tween_property(camera, "zoom", Vector2(0.5, 0.5), 1.0)
+	if camera.zoom != camera_zoom:
+		if zoom_tween:
+			zoom_tween.kill()
+		zoom_tween = create_tween()
+		zoom_tween.tween_property(camera, "zoom", camera_zoom, 1.0)
+
+
+func _on_longer_thread_powerup_collected() -> void:
 	void_chasing.start(player)
