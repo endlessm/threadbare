e6932267ff736dc80cd8719a2a6939f824985594
Will Thompson <wjt@endlessaccess.org>
2026-05-06 12:06:24 +0100

    Add tutorial quest
    Define a new LoreQuest, `quest_000`, which is triggered from the title
    screen rather than the previous intro sequence. This quest has a new NPC
    teach you the various mechanics of the game, with cut-down versions of
    each challenge. Eventually you reach Fray's End, just as with any other
    quest.
    
    This is a first iteration: I would expect several further rounds of
    scene-dressing and level design.
    
    When I was looking for a name for the NPC, I learnt that “Arun” means
    “dawn” in Sanskrit, which seemed apt for a first chapter. But I am not
    attached to the name.
    
    Resolves https://github.com/endlessm/threadbare/issues/2123
    
    Co-authored-by: Sarah Spiers <sarah@endlessaccess.org>
    Co-authored-by: Manuel Quiñones <manuq@endlessaccess.org>

---
+++ scenes/quests/lore_quests/quest_000/4_ink_combat/tutorial_ink_combat.gd
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Node2D
+
+@onready var repel_powerup: CharacterBody2D = %RepelPowerup
+@onready var fill_game_logic: FillGameLogic = %FillGameLogic
