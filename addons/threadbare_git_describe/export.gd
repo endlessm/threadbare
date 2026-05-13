2ef48617b7836c20207a825b32d95ffe6dfa3894
Will Thompson <wjt@endlessaccess.org>
2026-05-13 20:35:42 +0100

    Add Windows export preset
    Annoyingly this needed changes to the version-generator plugin, because
    exporting generated this warning:
    
        WARNING: Invalid version number "v0.1.11-72-g29be516f2". The version
        number can only contain numeric characters (0-9) and non-consecutive
        periods (.).
    
    I changed it so that "application/config/version" is "0.1.12" for the
    tag "v0.1.12", and "0.1.11.72" for a build 72 commits ahead of v0.1.11.
    The full `git describe --tags` output is now in
    "application/config/full_version".
    
    Explicitly qualify the export name with "x86_64". It is possible we will
    one day add an arm64 export. Update the Linux export accordingly. Ensure
    the target directory exists for both builds
    
    Resolves https://github.com/endlessm/threadbare/issues/2155

--- addons/threadbare_git_describe/export.gd
+++ addons/threadbare_git_describe/export.gd
@@ -9,18 +9,20 @@ func _get_name() -> String:
 	return "threadbare_git_describe_exporter"
 
 
-func set_version(version: Variant) -> void:
-	ProjectSettings.set_setting("application/config/version", version)
+func _export_begin(
+	_features: PackedStringArray, _is_debug: bool, _path: String, _flags: int
+) -> void:
+	var describe := Version.git_describe(true)
+	ProjectSettings.set_setting(Version.FULL_VERSION_KEY, describe)
+	ProjectSettings.set_setting(Version.VERSION_KEY, Version.simplify(describe))
 	var err := ProjectSettings.save()
 	if err != OK:
 		printerr("Failed to save project settings: %s" % error_string(err))
 
 
-func _export_begin(
-	_features: PackedStringArray, _is_debug: bool, _path: String, _flags: int
-) -> void:
-	set_version(Version.git_describe(true))
-
-
 func _export_end() -> void:
-	set_version(null)
+	ProjectSettings.set_setting(Version.FULL_VERSION_KEY, null)
+	ProjectSettings.set_setting(Version.VERSION_KEY, null)
+	var err := ProjectSettings.save()
+	if err != OK:
+		printerr("Failed to save project settings: %s" % error_string(err))
