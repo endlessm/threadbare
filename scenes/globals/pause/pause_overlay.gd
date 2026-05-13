a7176dea82fae3bc2a5d0d4c734e8fbfa99a5615
Will Thompson <wjt@endlessaccess.org>
2026-05-13 16:31:26 +0100

    Replace "Abandon Quest" with "Skip Tutorial" for tutorial quest
    Unlike abandoning a quest, when we skip the tutorial we must grant the
    player the abilities that they would have gained by completing the
    tutorial.
    
    This is a bit overengineered but it felt better to list the abilities in
    the tutorial's quest.tres rather than to hardcode them in the pause
    menu.
    
    Resolves https://github.com/endlessm/threadbare/issues/2160

--- scenes/globals/pause/pause_overlay.gd
+++ scenes/globals/pause/pause_overlay.gd
@@ -9,6 +9,7 @@ extends CanvasLayer
 @onready var resume_button: Button = %ResumeButton
 @onready var options: Control = %Options
 @onready var abandon_quest_button: Button = %AbandonQuestButton
+@onready var skip_tutorial_button: Button = %SkipTutorialButton
 
 
 func _ready() -> void:
@@ -29,7 +30,15 @@ func toggle_pause() -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_ARROW if new_state else Input.CURSOR_CROSS)
 
 	if new_state:
-		abandon_quest_button.visible = GameState.is_on_quest()
+		if not GameState.current_quest:
+			skip_tutorial_button.hide()
+			abandon_quest_button.hide()
+		elif GameState.current_quest.skippable:
+			skip_tutorial_button.show()
+			abandon_quest_button.hide()
+		else:
+			skip_tutorial_button.hide()
+			abandon_quest_button.show()
 		pause_menu.show()
 		resume_button.grab_focus()
 
@@ -37,6 +46,16 @@ func toggle_pause() -> void:
 func _on_abandon_quest_pressed() -> void:
 	toggle_pause()
 	GameState.abandon_quest()
+	SceneSwitcher.change_to_file_with_transition(
+		frays_end, ^"", Transition.Effect.FADE, Transition.Effect.FADE
+	)
+
+
+func _on_skip_tutorial_pressed() -> void:
+	toggle_pause()
+	for ability: Enums.PlayerAbilities in GameState.current_quest.skip_abilities:
+		GameState.set_ability(ability, true)
+	GameState.mark_quest_completed()
 	SceneSwitcher.change_to_file_with_transition(
 		frays_end, ^"", Transition.Effect.FADE, Transition.Effect.FADE
 	)
