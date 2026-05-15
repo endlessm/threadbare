791bf06d5ec61f8e3ec8f30931d52d27ac48a439
Will Thompson <wjt@endlessaccess.org>
2026-05-15 10:33:43 +0100

    DO NOT MERGE: Remove all StoryQuests
    This is part of pruning the project to make a smaller Backstitch project
    for a jam. It's not intended to be merged!

--- scenes/quests/story_quests/shjourney/3_shjourney_intro/area_2d.gd
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: The Threadbare Authors
-# SPDX-License-Identifier: MPL-2.0
-extends Area2D
-
-func _on_Area2D_body_entered(body: Node) -> void:
-	if body.name == "Player":
-		print("¡Jugador detectado!")
-		$"../Cinematic_siguiente".start_cinematic()
