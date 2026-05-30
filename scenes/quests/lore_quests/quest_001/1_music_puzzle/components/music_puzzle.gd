6e9aabc00a78b3bec785945bb8cb6e68542ce871
Will Thompson <wjt@endlessaccess.org>
2026-05-30 22:43:07 +0100

    Improve music puzzle
    Add background music to this scene: initially just a bass line and very
    small percussion part, which progresses as you complete each step of the
    puzzle. The full melody plays when you have completed the puzzle. Change
    the melody on the rocks to match. Include the strudel.cc source code to
    the music; the five files are simply slices of a 16-cycle export. The
    guitar arpeggios + kalimba + folk harp stack was me trying to imagine
    what the musician's weird three-necked instrument might sound like.
    
    The clips for each step of the puzzle are configured to loop from
    halfway through the clip, so that the gentle cymbal hit at the start
    only plays once per step. This currently doesn't work in the Godot web
    target but it sounds OK anyway.
    
    Increase the attenuation and reduce the volume on the ambient sounds so
    they're not so overwhelming when you're far away from the forest/river.
    Add an AudioListener to the player so that the distance is measured from
    the player, rather than the middle of the screen, which makes a
    big difference at the start of the level.
    
    The new melody is in A♭ major. I picked this key (and cheated by having
    the double bass play a low E♭, which can't be played without downtuning
    or having a C extension) because the combat music is in F minor, which
    is the relative minor of A♭ major.
    
    The melody incorporates the opening four notes of the theme,
    specifically as the third step of the puzzle.
    
    The rock scale starts on G (the 7th note of the scale) rather than A♭.
    It's a bit weird but it lets you play the full Threadbare theme melody
    (in B♭ dorian) with rocks 5 4 5 3 2 1, 5 4 5 3 5 6. I would like to add
    these as 2 further optional steps to the puzzle to unlock a new area of
    the level in future.
    
    Make the rock strike duration significantly shorter. This duration
    happens to be almost exactly a quaver (8th note) at 80bpm, the speed of
    the music. I thought about trying to sync the demo to the start of a
    bar, but it's not worth it.
    
    Adjust the musician's hints. I removed my pet peeve – that he would
    previously give you partial hints that were wrong:
    
    > Musician: Try tapping the first, and second stones, then check the
    > first altar above. If you see flames, we're on to the right melody.
    
    This is a bad hint because if you do what he says, nothing happens!
    
    Instead his clues are now more like:
    
    1. Listen to the altar./Look at the symbol.
    2. Explain the symbol.
    3. Give you the answer.
    
    For the final step, he tells you exactly what to do, but not the rocks.
    I think if someone's got that far they can work it out.

---
+++ scenes/quests/lore_quests/quest_001/1_music_puzzle/components/music_puzzle.gd
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Node2D
+
+@onready var background_music: BackgroundMusic = %BackgroundMusic
+
+
+func _on_sequence_puzzle_step_solved(step_index: int) -> void:
+	# This assumes that the clip indexes correspond to the puzzle indexes.
+	var stream := background_music.stream as AudioStreamInteractive
+	var next_clip := stream.get_clip_name(step_index + 1)
+	MusicPlayer.switch_to_clip(next_clip)
