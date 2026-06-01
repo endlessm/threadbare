fc14f3535c9cc5fc343f50a0ddab1cc82b592a6a
Manuel Quiñones <manuq@endlessaccess.org>
2026-06-01 20:07:23 -0300

    Musician quest: Add helpers
    Add the following helpers:
    - 1_music_puzzle: Memory helper that triggers a dialogue saying "Lore lore lore" as placeholder.
    - 2_ink_combat: Spirit helper that fills the hard to reach inkwell.
    - 3_stealth_level: Imagination helper that fixes the bridge.

---
+++ scenes/quests/lore_quests/quest_001/3_stealth_level/components/fix_bridge.gd
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Node
+
+const BRIDGE_TILESET_SOURCE_ID = 3
+const FIXED_BRIDGE_ATLAS_COORDS = Vector2i(0, 2)
+
+var _tiles_to_fix: Array[Vector2i] = [Vector2i(86, 21), Vector2i(108, 19)]
+
+@onready var bridges: TileMapLayer = $"../../TileMapLayers/Bridges"
+
+
+func fix_bridge() -> void:
+	for t in _tiles_to_fix:
+		bridges.set_cell(t, BRIDGE_TILESET_SOURCE_ID, FIXED_BRIDGE_ATLAS_COORDS)
+	await Engine.get_main_loop().create_timer(1).timeout
