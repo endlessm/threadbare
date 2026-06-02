8ce838ddd872a387516d526340fec45dcae61cd4
Manuel Quiñones <manuq@endlessaccess.org>
2026-06-02 12:08:35 -0300

    grappling_hook_needles: Copy camera limits before the animation
    Otherwise part of the scene outside the limits can be seen during the
    transition. Also:
    
    - Remove an unwanted comment
    - Add 1 second delay before displaying the dialogue again
    - Use sine transition for the tween resource of the PhantomCamera2D

--- scenes/quests/lore_quests/quest_002/2_grappling_hook/components/extra_needles.gd
+++ scenes/quests/lore_quests/quest_002/2_grappling_hook/components/extra_needles.gd
@@ -5,8 +5,6 @@ extends Node
 @export var extra_needles: Array[Node2D]
 @onready var helper_camera: PhantomCamera2D = %HelperCamera
 
-# signal needles_animation_finished
-
 
 func _ready() -> void:
 	for n in extra_needles:
@@ -15,6 +13,7 @@ func _ready() -> void:
 
 
 func add_extra_needles() -> void:
+	CameraUtilities.copy_current_camera_limits(helper_camera)
 	helper_camera.priority = 20
 	await get_tree().create_timer(1.0).timeout
 	for n in extra_needles:
@@ -22,3 +21,4 @@ func add_extra_needles() -> void:
 		n.process_mode = Node.PROCESS_MODE_INHERIT
 		await get_tree().create_timer(1.0).timeout
 	helper_camera.priority = 0
+	await get_tree().create_timer(1.0).timeout
