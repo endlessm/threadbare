43dc249e2f7f86fa693e136bec2a72fe2fc2ba31
NataliaOspinal <226923708+NataliaOspinal@users.noreply.github.com>
2026-06-05 15:04:55 -0500

    intro v1

--- scenes/ui_elements/cinematic/cinematic.gd
+++ scenes/ui_elements/cinematic/cinematic.gd
@@ -28,8 +28,16 @@ signal cinematic_finished
 ## Wether to automatically start the cinematic.
 @export var autostart: bool = true
 
+## La cámara del jugador que tomará el control al finalizar el diálogo.
+@export var player_camera: Camera2D 
+
+@export var actor_cinematica: Node2D
+@export var jugador_real: Node2D
 
 func _ready() -> void:
+	if jugador_real:
+		jugador_real.hide()
+		jugador_real.process_mode = Node.PROCESS_MODE_DISABLED
 	if autostart:
 		start()
 
@@ -40,6 +48,21 @@ func start() -> void:
 		await DialogueManager.dialogue_ended
 		cinematic_finished.emit()
 		GameState.intro_dialogue_shown = true
+		
+		if actor_cinematica:
+			actor_cinematica.queue_free()
+			
+		
+		if jugador_real:
+			if actor_cinematica:
+				jugador_real.global_position = actor_cinematica.global_position
+				
+			jugador_real.show()
+			jugador_real.process_mode = Node.PROCESS_MODE_INHERIT # Descongela al jugador
+			
+			var camara = jugador_real.get_node_or_null("Camera2D")
+			if camara:
+				camara.make_current()
 
 	if next_scene:
 		(
