1a4658dff6012d5caae6fc17fbc81c0ffb1254f5
Will Thompson <wjt@endlessaccess.org>
2026-06-10 15:42:43 +0100

    Split placeholder quest in two
    Separate the stealth/sequence combo from the ink combat trio. Give each
    a slightly better title and description.
    
    Add intro dialogue from the musician to each of the ink combat scenes to
    explain the new mechanic in each.
    
    Redesign the Song Sanctuary hub to have entrypoints for each.
    
    Adjust Bridget and the LoreQuest Elder's dialogue for the split,
    prioritising the Guarded Stones but allowing for the player having
    completed the quests in either order.
    
    While we are moving these files around anyway: some of the ink
    barrel-related files in this quest were referenced from scenes in
    scenes/game_elements/props/filling_barrel. Move these files out to that
    folder, so that generic components are not referencing files inside a
    quest.
    
    Resolves https://github.com/endlessm/threadbare/issues/2268

--- scenes/world_map/components/song_sanctuary_path.gd
+++ scenes/world_map/components/song_sanctuary_path.gd
@@ -4,5 +4,7 @@ extends Node2D
 
 # For dialogue etc.
 @onready var musician_quest_unlocker: QuestProgressUnlocker = %MusicianQuestUnlocker
+@onready var guarded_stones_unlocker: QuestProgressUnlocker = %GuardedStonesUnlocker
+@onready var oops_all_inkdrinkers_unlocker: QuestProgressUnlocker = %OopsAllInkDrinkersUnlocker
 
 # TODO: demodulate & fix houses as more song sanctuary quests are completed?
