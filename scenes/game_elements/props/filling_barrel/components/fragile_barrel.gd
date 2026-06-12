b9c60b297c8e6560740bc6bd2d757b5be4d255b5
Litraxx <fabien-goehl@web.de>
2026-06-12 23:10:38 +0200

    Refactor fragile barrel to use health component
    Changed break_barrel and take_damage functions to be called by health component signals
    Replaced existing health logic with health component

--- scenes/game_elements/props/filling_barrel/components/fragile_barrel.gd
+++ scenes/game_elements/props/filling_barrel/components/fragile_barrel.gd
@@ -7,50 +7,39 @@ extends FillingBarrel
 ## Emitted when [member current_health] reaches 0.
 signal barrel_destroyed(barrel_instance: FragileBarrel)
 
-## Maximum hits the barrel can take before breaking.
-@export_range(1, 100) var max_health: int = 4
-
-var current_health: int
-
 @onready var crack_overlay_node: AnimatedSprite2D = %CrackOverlay
 @onready var crack_sound: AudioStreamPlayer2D = %CrackSound
 @onready var shatter_sound: AudioStreamPlayer2D = %ShatterSound
+@onready var health_component: HealthComponent = %HealthComponent
 
 
 func _ready() -> void:
 	super._ready()
-	current_health = max_health
 	crack_overlay_node.visible = false
 
 
 # Logic called by Projectile when it hits this object
 func hit_by_droplet(droplet_label: String) -> void:
 	# Ignore if already full or destroyed
-	if _amount >= needed_amount or current_health <= 0:
+	if _amount >= needed_amount or health_component.has_depleted_health:
 		return
 
 	if droplet_label == self.label:
 		increment()
 	else:
-		take_damage()
+		health_component.damage(1)
 
 
-func take_damage() -> void:
-	current_health -= 1
-
-	if current_health <= 0:
-		break_barrel()
-	else:
+func take_damage(_current_health: int, has_depleted_health: bool) -> void:
+	if not has_depleted_health:
 		crack_sound.play()
 		update_cracks()
 
 
 func update_cracks() -> void:
-	var damage_taken: int = max_health - current_health
-
 	# IMPROVEMENT: Calculate frame index proportionally based on damage percentage.
 	var total_frames: int = crack_overlay_node.sprite_frames.get_frame_count("default")
-	var frame_index: int = int(floor((float(damage_taken) / max_health) * total_frames))
+	var frame_index: int = int(floor((health_component.damage_taken_percentage) * total_frames))
 
 	# Clamp to ensure we don't exceed available frames (0-based index)
 	frame_index = clamp(frame_index, 0, total_frames - 1)
