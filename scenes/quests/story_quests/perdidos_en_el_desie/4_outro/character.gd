be32ff5b01d6441d3cd089e1cb6a3941d363ffeb
Sebastian <sebastianbelli566@gmail.com>
2026-06-13 15:41:46 -0500

    Agregación de Animación de Movimiento

--- scenes/quests/story_quests/perdidos_en_el_desie/4_outro/character.gd
+++ scenes/quests/story_quests/perdidos_en_el_desie/4_outro/character.gd
@@ -7,17 +7,27 @@ func _ready() -> void:
 var speed:int = 120 #Velocidad de movimiento del personaje
 
 #Función de movimiento al lugar final del recorrido
-func mover_a_salida() -> void: 
+func mover_a_recuerdos() -> void: 
 	await get_tree().create_timer(2.0).timeout
 	play("walk") #Caminar hacia derecha
-	await mover_a(Vector2(500,200))
-	play("attack_03") #Practica de batalla
-	await get_tree().create_timer(3.0).timeout
-	play("walk") #Caminar hacia la salida
-	await mover_a(Vector2(670,200))
-	await mover_a(Vector2(670,470))
-	await mover_a(Vector2(1600,470))
+	await mover_a(Vector2(300,170))
 	play("idle")
+	await get_tree().create_timer(1.0).timeout
+	play("attack_03") #Practica de batalla
+	await get_tree().create_timer(0.5).timeout
+	play("idle")
+	await get_tree().create_timer(3.0).timeout
+	
+func mover_a_salida() -> void: 
+	play("walk") #Caminar hacia la salida
+	await mover_a(Vector2(540,170))
+	await mover_a(Vector2(540,470))
+	play("idle")
+	await get_tree().create_timer(1.0).timeout
+	play("walk")
+	await mover_a(Vector2(1250,470))
+	play("idle")
+	
 #Función a puntos espescificos
 func mover_a(destino:Vector2) -> void: 
 	while position.distance_to(destino) > 5 :
--- scenes/ui_elements/cinematic/cinematic.gd
+++ scenes/ui_elements/cinematic/cinematic.gd
@@ -6,6 +6,7 @@ extends Node2D
 signal cinematic_finished
  
 @export var dialogue: DialogueResource = preload("uid://b7ad8nar1hmfs")
+@export var dialogue_02: DialogueResource = preload("res://scenes/quests/story_quests/perdidos_en_el_desie/4_outro/outro_components/el_ultimo_adios.dialogue")
 
 ## Wether to automatically start the cinematic.
 @export var autostart: bool = true
@@ -17,26 +18,25 @@ func _ready() -> void:
 
 
 func start() -> void:
-	var diague = DialogueManager.show_dialogue_balloon(dialogue, "", [self])
-	var diague_ui:Control = diague.get_node("Balloon")
-	await get_tree().process_frame
-	await get_tree().process_frame
-  
-	var size_viewport:Vector2 = get_viewport().size
- 
-	diague_ui.position = Vector2(
-		size_viewport.x - 500, 20
-	)
-	
-	await DialogueManager.dialogue_ended
-	cinematic_finished.emit()
-	GameState.intro_dialogue_shown = true
-
-	var character:AnimatedSprite2D = get_node("../OnTheGround/Character")
+	var character:Node2D = get_node("../OnTheGround/Personaje")
+	await mostrar_dialogo(dialogue,true)
+	await character.mover_a_recuerdos()
+	await mostrar_dialogo(dialogue_02,false)
 	await character.mover_a_salida()
-	
+	GameState.intro_dialogue_shown = true
 	get_tree().quit()
 	
+func mostrar_dialogo(dialogo: DialogueResource,mover:bool)-> void:
+	var diague = DialogueManager.show_dialogue_balloon(dialogo, "", [self])
+	var diague_ui:Control = diague.get_node("Balloon")
+	await get_tree().process_frame
+	var size_viewport:Vector2 = get_viewport().size
+	if(mover):
+		diague_ui.position = Vector2(
+			size_viewport.x - 500, 20
+		)
+	await DialogueManager.dialogue_ended
+	cinematic_finished.emit()
 
  
 
