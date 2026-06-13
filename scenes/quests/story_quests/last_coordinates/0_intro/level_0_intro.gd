cb265da8800ca28b3e74d4dd253d7b4d300b012b
MateoP <unplayersacc@gmail.com>
2026-06-12 23:15:11 -0500

    Minijuego2 buen avance, ademas ya esta la intro

---
+++ scenes/quests/story_quests/last_coordinates/0_intro/level_0_intro.gd
@@ -0,0 +1,37 @@
+# Script used to move the bacgroun while dialogue runs
+
+extends Control
+
+var speed : float = 0.0
+@warning_ignore("narrowing_conversion")
+var y_length : int = 0.0
+@warning_ignore("narrowing_conversion")
+var x_length : int = 0.0
+
+## Camera
+@export var camera : Camera2D
+
+## Background
+@export var background : TextureRect
+
+## Duration in seconds of moving background
+@export var duration : float = 10.0
+
+
+
+func _ready() -> void:
+	y_length = (int)(background.size.y * background.scale.y)
+	x_length = (int)(background.size.x * background.scale.x)
+	# limiting the camera
+	camera.limit_bottom = y_length
+	camera.limit_right = x_length
+	speed = y_length / duration
+	pass
+	
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	camera.move_local_y(speed * delta)
+	
