ff4976f21a4d5427e7e1948587eae01ef41187e9
Luccio2005 <luisernestocruzgonzales1@gmail.com>
2026-06-16 11:36:34 -0500

    avance intro cinematica y assets

---
+++ scenes/quests/story_quests/phantom_toy/0_intro/oso.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite2D
+
+func play_default():
+	play("default")
+	
+func play_walk1():
+	play("walk_right")
+	
+func play_steady2():
+	play("steady_left")
---
+++ scenes/quests/story_quests/phantom_toy/0_intro/sombra.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2D
+func play_sale():
+	play("sale")
+	
+func play_parado():
+	play("parado")
+
+func play_camina():
+	play("camina")
