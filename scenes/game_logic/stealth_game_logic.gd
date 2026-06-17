6b76e4e38db868e1c4c6ea66e5a1dc2195f678f6
kevinKVN00 <kevincho.ggr@hotmail.com>
2026-06-17 13:47:53 -0500

    modificacion1 y 2

--- scenes/game_logic/stealth_game_logic.gd
+++ scenes/game_logic/stealth_game_logic.gd
@@ -4,6 +4,8 @@
 class_name StealthGameLogic
 extends Node
 
+@export var obstaculo:Node2D
+@export var camara:Sprite2D
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
@@ -17,3 +19,17 @@ func _on_player_detected(player: Node2D) -> void:
 		player.defeat()
 	else:
 		push_warning("Detected node does not have defeat() method", player)
+
+var jugador_en_rango = false
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	jugador_en_rango = true
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("throw"):
+		if jugador_en_rango: 
+			if obstaculo: 
+				obstaculo.queue_free()
+				camara.visible=true
+		
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	jugador_en_rango = false
