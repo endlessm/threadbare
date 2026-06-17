d29b31f8fac8ad148343fd46e206de6d02928ca4
Sebastian <sebastianbelli566@gmail.com>
2026-06-17 18:38:15 -0500

    Agregación de decoraciones

--- scenes/ui_elements/cinematic/cinematic.gd
+++ scenes/ui_elements/cinematic/cinematic.gd
@@ -4,9 +4,13 @@ class_name Cinematic
 extends Node2D
  
 signal cinematic_finished
+
+@onready var fade_rect = $ColorRect
+@onready var anim = $AnimationPlayer
  
 @export var dialogue: DialogueResource = preload("uid://b7ad8nar1hmfs")
 @export var dialogue_02: DialogueResource = preload("res://scenes/quests/story_quests/perdidos_en_el_desie/4_outro/outro_components/el_ultimo_adios.dialogue")
+
 
 ## Wether to automatically start the cinematic.
 @export var autostart: bool = true
@@ -18,15 +22,24 @@ func _ready() -> void:
 
 
 func start() -> void:
+	# Animación de cierre
+	fade_rect.self_modulate.a = 0
+	#Para la animación del personaje
 	var character:Node2D = get_node("../OnTheGround/Personaje")
+	# Pantalla en blanco
 	await mostrar_dialogo(dialogue,true)
 	await character.mover_a_recuerdos()
 	await mostrar_dialogo(dialogue_02,false)
 	await character.mover_a_salida()
 	GameState.intro_dialogue_shown = true
+	
+	# Función de cierre 
+	anim.play("fade_out")
+	await get_tree().create_timer(2.0).timeout
 	get_tree().quit()
 	
 func mostrar_dialogo(dialogo: DialogueResource,mover:bool)-> void:
+	@warning_ignore("untyped_declaration")
 	var diague = DialogueManager.show_dialogue_balloon(dialogo, "", [self])
 	var diague_ui:Control = diague.get_node("Balloon")
 	await get_tree().process_frame
@@ -37,6 +50,10 @@ func mostrar_dialogo(dialogo: DialogueResource,mover:bool)-> void:
 		)
 	await DialogueManager.dialogue_ended
 	cinematic_finished.emit()
+	
+	# Función de cierre
+ 
+		
 
  
 
