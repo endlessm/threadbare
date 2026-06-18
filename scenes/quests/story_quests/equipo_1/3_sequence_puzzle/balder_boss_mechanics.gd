12420c5aa6285e862c8bdd49a69211dd601835ef
TheWeebCrafter10010 <alonso89benito2@gmail.com>
2026-06-18 10:52:26 -0500

    agregando mecanica de disparo de barrido

--- scenes/quests/story_quests/equipo_1/3_sequence_puzzle/balder_boss_mechanics.gd
+++ scenes/quests/story_quests/equipo_1/3_sequence_puzzle/balder_boss_mechanics.gd
@@ -4,6 +4,8 @@ extends Node
 @onready var boss = %BalderFuturo
 @onready var mapa = %Sand
 
+@onready var pos_inicial_disparos=%EnemigosDisparos.global_position
+@onready var pos_inicial_targets = %PlayerTargets.global_position
 enum Tipo {
 	NORTE,
 	SUR,
@@ -63,7 +65,6 @@ func _ejecutar_ataque() -> void:
 
 func _atacar_efecto(ataques: int, casilla_a_mover: Vector2i, direccion: Tipo) -> void:
 	boss.global_position = mapa.map_to_local(casilla_a_mover)
-	
 	# 2. Ahora sí, ejecuta su ataque por defecto (Disparo 1)
 	boss._on_timeout()
 	await boss.animation_player.animation_finished
@@ -84,6 +85,77 @@ func _atacar_efecto(ataques: int, casilla_a_mover: Vector2i, direccion: Tipo) ->
 		boss.shoot_projectile_at(player)
 		# Esperamos el pequeño bache de tiempo antes del siguiente tiro
 		await get_tree().create_timer(0.3).timeout
+		
+func ataque_barrido_prueba()->void:
+	ataque_direcciones(Tipo.NOR_OESTE)
+
+func ataque_direcciones(direccion:Tipo)->void:
+	var direccion_enemigos = direccion_patron(direccion)
+	
+	##Seran 4 posiciones de enemigos para simular 5 disparos (4 + el boss)
+	##los enemigos se generaran alado del jefe y si la direccion es una diagonal
+	##como diagonal
+	var posicion_boss = mapa.local_to_map(mapa.to_local(boss.global_position))
+	var posicion_player = mapa.local_to_map(mapa.to_local(player.global_position))
+	
+	var posiciones_disparos = patron_posiciones(posicion_boss,direccion_enemigos)
+	var posiciones_target = patron_posiciones(posicion_player,direccion_enemigos)
+	
+	var targets = %PlayerTargets.get_children();
+	
+	var disparos = %EnemigosDisparos.get_children();
+	
+	##si o si deben ser 4 disparos y 4 targets, sino crashea xd
+	colocar_nodos(posiciones_disparos,disparos)
+	colocar_nodos(posiciones_target,targets)
+	
+	##una vez colocado disparamos
+	var i = 0
+	for d in disparos:
+		d.shoot_projectile_at(targets[i])
+		i = i+1
+		
+func colocar_nodos(posiciones:Array[Vector2i],nodos:Array[Node])->void:
+	var i = 0
+	for n:Node2D in nodos:
+		var pos = mapa.to_global(mapa.map_to_local(posiciones[i]))
+		n.global_position = pos
+		i = i+1
+
+func reset_nodos(nodos:Array[Node], posicion_inicial:Vector2i):
+	for n:Node2D in nodos:
+		n.global_position = posicion_inicial
+		
+func direccion_patron(direccion:Tipo)->Vector2i:
+	var direccion_mover = Vector2i(0,0);
+	match direccion:
+		Tipo.NORTE, Tipo.SUR:
+			direccion_mover = Vector2i(1, 0)
+		Tipo.ESTE, Tipo.OESTE:
+			direccion_mover = Vector2i(0, 1)
+		Tipo.NOR_ESTE, Tipo.SUR_OESTE:
+			direccion_mover = Vector2i(1, 1)
+		Tipo.NOR_OESTE, Tipo.SUR_ESTE:
+			direccion_mover = Vector2i(-1, 1)
+	return direccion_mover		
+
+func patron_posiciones(posicion,direccion)->Array[Vector2i]:
+	##la posicion debe ser usando maptolocal
+	var posicion_iterada = posicion
+	var posiciones_nuevas : Array[Vector2i] = []
+	
+	for e in 2:
+		posicion_iterada = posicion_iterada+direccion
+		posiciones_nuevas.push_front(posicion_iterada)
+		
+	posicion_iterada = posicion
+	direccion = direccion * (-1)
+	
+	for e in 2:
+		posicion_iterada = posicion_iterada+direccion
+		posiciones_nuevas.push_front(posicion_iterada)
+	
+	return posiciones_nuevas
 
 func _pausar()->void:
 	boss.timer.stop();
