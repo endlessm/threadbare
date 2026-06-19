1ccf4a140db7f48e86bd6d88a99ebcb5b613794a
Leo <leyito2003@gmail.com>
2026-06-19 15:21:44 -0500

    completado

--- scenes/quests/story_quests/en_la_prondidad/2_combat/combat_components/fill_game_logicEnlaprondidad.gd
+++ scenes/quests/story_quests/en_la_prondidad/2_combat/combat_components/fill_game_logicEnlaprondidad.gd
@@ -1,7 +1,5 @@
-
 class_name CustomStealthGameLogic
 extends Node
-
 signal goal_reached
 
 @export var jefe: Node
@@ -10,8 +8,10 @@ signal goal_reached
 var goal_already_reached: bool = false
 
 func _ready() -> void:
-	if jefe != null and jefe.has_signal("jefe_muerto"):
-		jefe.connect("jefe_muerto", Callable(self, "_on_jefe_muerto"))
+	if jefe != null and jefe.has_signal("combate_completado"):
+		jefe.combate_completado.connect(_on_jefe_derrotado)
+	else:
+		push_warning("El jefe no tiene la señal combate_completado o no fue asignado.")
 
 	if autostart:
 		start()
@@ -21,9 +21,13 @@ func start() -> void:
 	get_tree().call_group("throwing_enemy", "start")
 
 
-func _on_jefe_muerto() -> void:
+func _on_jefe_derrotado() -> void:
 	if goal_already_reached:
 		return
 
 	goal_already_reached = true
+
+	get_tree().call_group("throwing_enemy", "remove")
+	get_tree().call_group("projectiles", "remove")
+
 	goal_reached.emit()
