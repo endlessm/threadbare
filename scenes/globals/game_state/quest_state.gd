3c7460819cfca6e92285f4f91f864ecf435d16ae
Manuel Quiñones <manuq@endlessaccess.org>
2026-06-23 10:38:53 -0300

    GameState: Add global and per-quest generic facts
    A generic database of facts about the world. Quests also have their own, for
    per-quest facts.
    
    Resolve https://github.com/endlessm/threadbare/issues/2279
    
    Co-authored-by: Will Thompson <wjt@endlessaccess.org>

--- scenes/globals/game_state/quest_state.gd
+++ scenes/globals/game_state/quest_state.gd
@@ -5,6 +5,9 @@ extends Resource
 
 @export var quest: Quest
 
+## Generic game state facts. Outside quests, use [member GlobalState.facts] instead.
+@export var facts: Dictionary[String, Variant]
+
 ## Resource path of [member quest], which is what is actually saved to disk.
 ## Use [member quest] instead.
 @export_storage var quest_path: String:
