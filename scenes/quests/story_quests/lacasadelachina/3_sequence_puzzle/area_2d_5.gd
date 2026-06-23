ae766785ffd53da3637c8841b45518d63f136c3f
emilysaavedra1781-pixel <emilysaavedra.17.8.1@gmail.com>
2026-06-22 23:23:00 -0500

    Puliendo el minijuego3

--- scenes/quests/story_quests/lacasadelachina/3_sequence_puzzle/area_2d_5.gd
+++ scenes/quests/story_quests/lacasadelachina/3_sequence_puzzle/area_2d_5.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 func _ready():
-	$AnimationPlayer.play("uno")
+	$AnimationPlayer.play("cinco")
 	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
 
 func _on_animation_finished(anim_name):
-	$AnimationPlayer.play("uno")
+	$AnimationPlayer.play("cinco")
 
 func _on_body_entered(body):
 	get_tree().reload_current_scene()
--- scenes/quests/story_quests/lacasadelachina/3_sequence_puzzle/rigid_body_2d_4.gd
+++ scenes/quests/story_quests/lacasadelachina/3_sequence_puzzle/rigid_body_2d_4.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 func _ready():
-	$AnimationPlayer.play("uno")
+	$AnimationPlayer.play("dos")
 	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
 
 func _on_animation_finished(anim_name):
-	$AnimationPlayer.play("uno")
+	$AnimationPlayer.play("dos")
 
 func _on_body_entered(body):
 	get_tree().reload_current_scene()
