884e91f48153baf9e0d541c46aafbbe00e22e4cf
Karm <benjamin.jk1408@gmail.com>
2026-06-23 14:17:12 -0500

    agregado outro y añadi cosas nvl 2

---
+++ scenes/quests/story_quests/legends_without_name/4_outro/outro.gd
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.play("outro")
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+	
