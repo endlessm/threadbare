4dec5da9bb6a589fe8a2bb7c99a55c2031f308a3
andreagit1 <andreavahp@gmail.com>
2026-06-24 12:31:26 -0500

    Diseño combat actualizado

---
+++ scenes/quests/story_quests/3_patitas/2_combat/3_patitas_combat.gd
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+
+extends Node2D
+
+func _ready() -> void:
+	# Tu lógica inicial para activar la escena sin depender de managers externos
+	pass
