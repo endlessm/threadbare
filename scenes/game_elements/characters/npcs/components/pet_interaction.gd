bb5c2bff702c0c5ed1dc61ec65527f988492f87b
Manuel Quiñones <manuq@endlessaccess.org>
2026-06-24 14:25:01 -0300

    Add achievement counter for petting cats and annoying sheeps
    Add an AchievementCounter node with a prefix that can persist a counter as
    global fact.
    
    Use it in cat and sheep scenes.
    
    Note that this will count multiple times for the same animal, as many as the
    player interacts. For counting if each specific animal was petted, a different
    approach (more similar to the upcoming button collected fact) is needed.
    
    Helps https://github.com/endlessm/threadbare/issues/2279

--- scenes/game_elements/characters/npcs/components/pet_interaction.gd
+++ scenes/game_elements/characters/npcs/components/pet_interaction.gd
@@ -5,6 +5,7 @@ extends Node
 @export var interact_area: InteractArea
 @export var sound_effect_player: AudioStreamPlayer2D
 @export var shaker: Shaker
+@export var achievement_counter: AchievementCounter
 
 
 func _ready() -> void:
@@ -17,5 +18,7 @@ func _on_interact_area_interaction_started(_player: Player, _from_right: bool) -
 	sound_effect_player.play()
 	if shaker:
 		shaker.shake()
+	if achievement_counter:
+		achievement_counter.add_achievement()
 	await sound_effect_player.finished
 	interact_area.disabled = false
---
+++ scenes/game_logic/achievement_counter/achievement_counter.gd
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+class_name AchievementCounter
+extends Node
+
+@export var prefix: String
+
+
+func add_achievement() -> void:
+	if not prefix:
+		push_warning("AchievementCounter.add_achievement() was called without a prefix.")
+		return
+	var fact_name := "%s_count" % prefix
+	if fact_name not in GameState.global.facts:
+		GameState.global.facts[fact_name] = 1
+	else:
+		GameState.global.facts[fact_name] += 1
