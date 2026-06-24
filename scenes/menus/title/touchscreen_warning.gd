8c9404604f507ca969a8e44a3b16fb52ca86044a
Alonna <145865677+alcole2@users.noreply.github.com>
2026-06-24 13:44:34 -0400

    Add full-screen touchscreen warning to TabContainer pages

---
+++ scenes/menus/title/touchscreen_warning.gd
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Control
+
+@onready var dismiss_button: Button = $PanelContainer/VBoxContainer/DismissButton
+
+
+func _ready() -> void:
+	# Connecting the button click
+	dismiss_button.pressed.connect(_on_dismiss_pressed)
+
+	# Checking if the device works for the game
+	if not _should_show_touchscreen_warning():
+		# If their device is compatible and they don't need the warning, jump straight to the main menu
+		_go_to_main_menu()
+
+
+func _should_show_touchscreen_warning() -> bool:
+	var has_touch: bool = DisplayServer.is_touchscreen_available()
+	var has_keyboard: bool
+	if OS.has_feature("web_android") or OS.has_feature("web_ios"):
+		# DisplayServer.has_hardware_keyboard() is not implemented on the web.
+		# Assume mobile web browsers have no hardware keyboard.
+		has_keyboard = false
+	else:
+		has_keyboard = DisplayServer.has_hardware_keyboard()
+	return has_touch and not has_keyboard
+
+
+func _on_dismiss_pressed() -> void:
+	_go_to_main_menu()
+
+
+func _go_to_main_menu() -> void:
+	# Getting the parent TabContainer node ('Pages')
+	var pages_container = get_parent() as TabContainer
+
+	if pages_container:
+		# Tab 0 is this warning screen so tab 1 is the main menu scene right below it
+		pages_container.current_tab = 1
