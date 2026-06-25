d31f0e621429d2cee010189ce53f599e685c2a54
Karm <benjamin.jk1408@gmail.com>
2026-06-25 03:44:17 -0500

    revision de dialogos

--- scenes/quests/story_quests/legends_without_name/4_outro/outro.gd
+++ scenes/quests/story_quests/legends_without_name/4_outro/outro.gd
@@ -5,9 +5,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 
-#func _ready() -> void:
-	#$AnimationPlayer.play("outro")
-	#pass  # Replace with function body.
+func _ready() -> void:
+	$AnimationPlayer.play("outro")
+
+	pass  # Replace with function body.
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://scenes/world_map/frays_end.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
