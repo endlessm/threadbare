27891f73f2917fd072e6aba186d3cdc001e249ce
Will Thompson <wjt@endlessaccess.org>
2026-06-25 14:29:28 +0100

    Split touchscreen warning to a separate scene
    This matches the other pages in title_screen. Connect the "pressed"
    signal of the button in the scene file rather than code.
    
    Following the "signal up, call down" pattern, it is not conventional to
    have the touchscreen warning node (which is a child of the title screen)
    call up in to the title screen to change the visible tab when dismissed.
    Take a slightly different approach:
    
    1. Make the main menu be the default-visible node in the TabContainer
    2. In the _ready() function for the touchscreen warning, make the
       warning visible (which has the effect of switching to this tab) if
       necessary.
    3. When dismissed, call hide(), which causes the TabContainer to show
       the next tab.
    
    Tweak the layout of the touchscreen warning:
    
    - Change the layout mode to "center";
    - Enable word wrapping on the label;
    - Set its minimum width to 640px.
    
    I hope this will make it fit on a portrait mode smartphone screen!

---
+++ scenes/menus/title/components/touchscreen_warning.gd
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Control
+
+
+func _ready() -> void:
+	if _should_show_touchscreen_warning():
+		show()
+
+
+func _should_show_touchscreen_warning() -> bool:
+	var has_touch: bool = DisplayServer.is_touchscreen_available()
+	var has_keyboard: bool
+	if OS.has_feature("web_android") or OS.has_feature("web_ios"):
+		# DisplayServer.has_hardware_keyboard() is not implemented on the web.
+		# Assuming mobile web browsers don't have a hardware keyboard.
+		has_keyboard = false
+	else:
+		has_keyboard = DisplayServer.has_hardware_keyboard()
+	return has_touch and not has_keyboard
+
+
+func _on_dismiss_button_pressed() -> void:
+	hide()
