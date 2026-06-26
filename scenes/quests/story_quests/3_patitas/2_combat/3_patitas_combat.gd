0aec17e1fb8500ccefcd480e39663ace7698841e
saltinzzz <stalindx1@gmail.com>
2026-06-26 11:35:08 -0500

    Juego completado y revisado

--- scenes/quests/story_quests/3_patitas/2_combat/3_patitas_combat.gd
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: The Threadbare Authors
-# SPDX-License-Identifier: MPL-2.0
-
-extends Node2D
-
-func _ready() -> void:
-	# Tu lógica inicial para activar la escena sin depender de managers externos
-	pass
