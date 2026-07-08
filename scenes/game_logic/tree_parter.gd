5cd2af14cce73c3c7a875b6d175b74c408ca694d
felixwalberg <fwalberg@uvm.edu>
2026-07-08 14:33:43 -0400

    Add top border to tree_parter to avoid body_exit call on teleportation

--- scenes/game_logic/tree_parter.gd
+++ scenes/game_logic/tree_parter.gd
@@ -8,22 +8,10 @@ const TREE_SCENE = preload("res://scenes/game_elements/props/tree/components/tre
 const SECRET_PATH_MODULATION = Color(.6, .6, .6, 0.3)
 
 @export_range(10, 1000, 10) var width: float = 200.0:
-	set(val):
-		if walkable_trees:
-			for shape in walkable_trees.get_children():
-				shape.shape.size.x = val
-			l_border.position.x -= (val - width) / 2
-			r_border.position.x += (val - width) / 2
-			width = val
+	set = _set_width_values
 
 @export_range(10, 1000, 10) var height: float = 400.0:
-	set(val):
-		if walkable_trees:
-			for shape in walkable_trees.get_children():
-				shape.shape.size.y = val
-			l_border.shape.size.y += (val - height) / 2
-			r_border.shape.size.y += (val - height) / 2
-			height = val
+	set = _set_height_values
 
 var trees: Array[Node2D]  # objects that should be modulated and not collide
 var colliders: Array[Node2D]  # objects that should not collide
@@ -31,8 +19,29 @@ var colliders: Array[Node2D]  # objects that should not collide
 @onready var walkable_trees: Area2D = $WalkableTrees
 @onready var l_border: CollisionShape2D = $Borders/LeftBorder
 @onready var r_border: CollisionShape2D = $Borders/RightBorder
+@onready var t_border: CollisionShape2D = $Borders/TopBorder
 # TODO: Give audio stream a leaf rustle sound
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+
+func _set_width_values(val: float) -> void:
+	if walkable_trees:
+		for shape in walkable_trees.get_children():
+			shape.shape.size.x = val
+		l_border.position.x -= (val - width) / 2
+		r_border.position.x += (val - width) / 2
+		t_border.shape.size.x += (val - width)
+		width = val
+
+
+func _set_height_values(val: float) -> void:
+	if walkable_trees:
+		for shape in walkable_trees.get_children():
+			shape.shape.size.y = val
+		l_border.shape.size.y += (val - height) / 2
+		r_border.shape.size.y += (val - height) / 2
+		t_border.position.y -= (val - height) / 2
+		height = val
 
 
 func _on_body_entered(body: Node2D) -> void:
