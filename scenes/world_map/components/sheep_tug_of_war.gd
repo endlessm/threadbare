198821873930c9725545ef082915c44bc82d62bc
felixwalberg <fwalberg@uvm.edu>
2026-07-21 10:53:14 -0400

    Clean up and document sheep_tug_of_war script

--- scenes/world_map/components/sheep_tug_of_war.gd
+++ scenes/world_map/components/sheep_tug_of_war.gd
@@ -13,23 +13,26 @@ var hook_control_2: Node2D
 @onready var hookable_box: AnimatableBody2D = $HookableBox
 
 
-## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# HookControl is interactive and all instances will respond
+	# to user input (aiming and throwing). Remove from `hook_listener`
+	# group so they don't react to user inputs.
 	player_hook.remove_from_group("hook_listener")
 	player_hook_2.remove_from_group("hook_listener")
 	hook_control_1 = player_hook.find_child("HookControl")
 	hook_control_2 = player_hook_2.find_child("HookControl")
-	hook_control_1.state = 0
-	hook_control_2.state = 0
+	hook_control_1.state = HookControl.State.DISABLED
+	hook_control_2.state = HookControl.State.DISABLED
 
 
-## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	# TODO: Better way to avoid getting input from player aim/controls?
-	if hook_control_1.state == 1:
-		hook_control_1.state = 0
-	if hook_control_2.state == 1:
-		hook_control_2.state = 0
+	# If HookControl is AIMING, cursor movement and keyboard
+	# input will change the player's aim and also the sheep.
+	# Disable so it only reflects on the player's aim.
+	if hook_control_1.state == HookControl.State.AIMING:
+		hook_control_1.state = HookControl.State.DISABLED
+	if hook_control_2.state == HookControl.State.AIMING:
+		hook_control_2.state = HookControl.State.DISABLED
 
 
 func _on_timer_timeout() -> void:
@@ -40,6 +43,6 @@ func _on_timer_timeout() -> void:
 
 	first_sheep.hooked(hookable_box.find_child("HookableArea"), true)
 
-	await get_tree().create_timer(max(randf() * MAX_DELAY, MIN_DELAY)).timeout
+	await get_tree().create_timer(randf_range(MIN_DELAY, MAX_DELAY)).timeout
 
 	second_sheep.hooked(hookable_box.find_child("HookableArea"), true)
