9dd66cebe8cc21367aa8a76120ec924998ee92db
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-22 17:52:40 -0300

    World being reweaven animation tests

---
+++ scenes/game_elements/fx/world_reweaven/components/world_reweaven_test.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+@onready var shaker: Shaker = %Shaker
+
+
+func _ready() -> void:
+	await get_tree().create_timer(2.).timeout
+	shaker.shake()
+	animation_player.play("reweaven")
+	await animation_player.animation_finished
+	SceneSwitcher.reload_with_transition(Transition.Effect.FADE, Transition.Effect.FADE)
