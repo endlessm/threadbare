844f6c0d2338edfca1c479310a43fb02fd8d3a65
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-30 16:23:26 -0300

    A bit more cleanup and polish

--- scenes/game_elements/fx/curly_trail/components/curly_trail.gd
+++ scenes/game_elements/fx/curly_trail/components/curly_trail.gd
@@ -4,22 +4,38 @@ class_name CurlyTrail
 extends Node2D
 ## @experimental
 ##
-## MANUQ TODO
+## A curly trail effect for a [Line2D] node.
 ##
-## MANUQ TODO
+## Add points to the line in circle, so when this node moves, a curly effect is obtained. The circle
+## radius becomes smaller the faster this node moves, as a simple way to achieve a thread stretching
+## effect.
+## [br][br]
+## Note: For the effect to be achieved, this node must move. For example, by putting it as child of
+## a [PathFollow2D] node.
 
+## The line to use for the effect.
 @export var line: Line2D
+
+## The radius at zero velocity.
 @export_range(10, 50, 1, "or_greater", "or_less") var radius: float = 30
-@export_range(0, 100, 1, "or_greater", "or_less") var max_vel: float = 50
+
+## The velocity at which the thread stretches fully.
+@export_range(0, 100, 1, "or_greater", "or_less") var stretched_velocity: float = 50
+
 @export_range(-45, 45, 0.1, "radians", "or_greater", "or_less") var wheel_rotation: float = 0.2
 @export_range(0, 1, 0.01) var radius_update: float = 0.1
 @export var min_points_distance := 40
+
+## How many points can the [member line] have.
 @export var max_points := 100
+
+## Draw the wheel circle and the tip for debugging.
 @export var debug := false
 
-var d := 0.0
-var last_pos: Vector2
-var noise := FastNoiseLite.new()
+## A value between 0 and 1 that is how stretched is the line.
+var _stretching_amount := 0.0
+
+var _last_position: Vector2
 
 @onready var wheel: Node2D = %Wheel
 @onready var tip: Node2D = %Tip
@@ -27,8 +43,6 @@ var noise := FastNoiseLite.new()
 
 func _ready() -> void:
 	tip.position.x = radius
-	noise.seed = 123
-	noise.frequency = 0.0005
 
 
 func _draw() -> void:
@@ -38,29 +52,28 @@ func _draw() -> void:
 
 
 func _process(_delta: float) -> void:
-	var diff := Vector2.ZERO if not last_pos else last_pos - global_position
-	last_pos = global_position
+	var direction := Vector2.ZERO if not _last_position else _last_position - global_position
+	_last_position = global_position
 
-	var x: float = (max_vel - min(diff.length_squared(), max_vel)) / max_vel
-	d = lerpf(d, x, radius_update)
+	var new_stretching_amount: float = (
+		(stretched_velocity - min(direction.length_squared(), stretched_velocity))
+		/ stretched_velocity
+	)
+	_stretching_amount = lerpf(_stretching_amount, new_stretching_amount, radius_update)
 
-	# var dx: float = noise.get_noise_1d(Time.get_ticks_msec() * 1) * 50 * d
-	tip.position.x = radius * d  #  + dx
+	tip.position.x = radius * _stretching_amount
+	wheel.rotate(wheel_rotation * _stretching_amount * _stretching_amount)
 
-	wheel.rotate(wheel_rotation * d * d)
-	trail(tip.global_position)
-	if debug:
-		queue_redraw()
-
-
-func trail(global_pos: Vector2) -> void:
 	if not line.get_point_count():
-		line.add_point(line.to_local(global_pos))
+		line.add_point(line.to_local(tip.global_position))
 	else:
 		var last_p := line.points[-1]
-		if last_p.distance_squared_to(global_pos) < min_points_distance:
+		if last_p.distance_squared_to(tip.global_position) < min_points_distance:
 			line.remove_point(0)
 			return
-		line.add_point(line.to_local(global_pos))
+		line.add_point(line.to_local(tip.global_position))
 	while line.get_point_count() > max_points:
 		line.remove_point(0)
+
+	if debug:
+		queue_redraw()
--- scenes/game_elements/fx/stitch_trail/components/stitch_trail.gd
+++ scenes/game_elements/fx/stitch_trail/components/stitch_trail.gd
@@ -4,60 +4,68 @@ class_name StitchTrail
 extends Node2D
 ## @experimental
 ##
-## MANUQ TODO
+## A stitching effect for a [Line2D] node.
 ##
-## MANUQ TODO
+## Add points to the line in zigzag, imitating a thread being stitched.
+## [br][br]
+## Note: For the effect to be achieved, this node must move. For example, by putting it as child of
+## a [PathFollow2D] node.
 
+## The line to use for the effect.
 @export var line: Line2D
-@export_range(0, 150, 1.0, "suffix:px") var width: float = 50
-@export_range(0, 1, 0.01) var stitches_per_second: float = 0.03
-@export_range(0, 1, 0.01) var direction_update: float = 0.1
+
+## Flatten the effect vertically so it looks like it's on the floor, matching
+## the Threadbare top-down isometric perspective.
 @export var on_floor: bool = true
+
+## How thick is the effect.
+@export_range(0, 150, 1.0, "suffix:px") var width: float = 50
+
+## How many stitches per second.
+@export_range(0, 1, 0.01) var stitches_per_second: float = 0.03
+
+## Weight of the direction lerp interpolation. To smooth the zigzag direction as this node moves.
+## This wouldn't be necessary if moving along a predefined path. But if moving abruptly using the
+## mouse position (for instance) this smoothing is better.
+@export_range(0, 1, 0.01) var direction_weight: float = 0.1
+
+## MANUQ TODO
 @export var min_points_distance := 40
+
+## How many points can the [member line] have.
 @export var max_points := 100
 
-var last_stitch_seconds: float = 0
-var last_pos: Vector2
-var stitch_direction: Vector2
-var stitch_sign := 1
+var _last_stitch_seconds: float = 0
+var _last_position: Vector2
+var _stitch_direction: Vector2
+var _stitch_sign := 1
+
+
+func _do_stitch() -> void:
+	var new_point := global_position + (_stitch_direction * width / 2) * _stitch_sign
+	line.add_point(line.to_local(new_point))
+	_stitch_sign *= -1
+	_last_position = global_position
 
 
 func _process(delta: float) -> void:
-	var diff := Vector2.ZERO if not last_pos else last_pos - global_position
-	var normal := diff.orthogonal().normalized()
-	if on_floor:
-		normal *= Vector2(1, 0.5)
-	stitch_direction = lerp(stitch_direction, normal, direction_update)
-
-	if last_stitch_seconds < stitches_per_second:
-		last_stitch_seconds += delta
+	if _last_stitch_seconds < stitches_per_second:
+		_last_stitch_seconds += delta
 		return
-	last_stitch_seconds = 0
+	_last_stitch_seconds = 0
 
-	trail(global_position)
+	var direction := Vector2.ZERO if not _last_position else _last_position - global_position
+	var orthogonal := direction.orthogonal().normalized()
+	if on_floor:
+		orthogonal *= Vector2(1, 0.5)
+	_stitch_direction = _stitch_direction.lerp(orthogonal, direction_weight)
 
-
-func trail(global_pos: Vector2) -> void:
-	var new_p := global_position + (stitch_direction * width / 2) * stitch_sign
 	if not line.get_point_count():
-		line.add_point(line.to_local(new_p))
-		stitch_sign *= -1
-		last_pos = global_position
+		_do_stitch()
 	else:
-		if last_pos.distance_squared_to(global_pos) < min_points_distance:
+		if _last_position.distance_squared_to(global_position) < min_points_distance:
 			line.remove_point(0)
 			return
-		line.add_point(line.to_local(new_p))
-		stitch_sign *= -1
-		last_pos = global_position
+		_do_stitch()
 	while line.get_point_count() > max_points:
 		line.remove_point(0)
-
-
-func _notification(what: int) -> void:
-	match what:
-		NOTIFICATION_ENABLED:
-			last_pos = Vector2.ZERO
-			last_stitch_seconds = 0
-			stitch_direction = Vector2.ZERO
-			stitch_sign = 1
