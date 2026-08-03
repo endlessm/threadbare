7ed802afbe528cb68d061e01d21454de56e50a1d
Will Thompson <wjt@endlessaccess.org>
2026-08-03 13:22:23 +0100

    Push StoryQuest kit as Git tag
    The goal here is that, if we want to import a StoryQuest from Backstitch
    back into Git, we can do so as follows:
    
    1. Look in the project settings which Git version the StoryQuest kit
       came from;
    2. Check out the tag for that kit;
    3. Replace all files in the directory (except `.git`) with the
       Backstitch project;
    4. Commit.
    5. Now we have a clean commit for "add new StoryQuest" which we can
       rebase onto upstream main.
    
    This would also be a useful thing to have if we want to continue to
    support running Core: Threadbare in Git(Hub). Two ideas that would both
    be better than what we have today:
    
    1. Automatically sync these tags to the `main` branch of a separate
       `threadbare-storyquest-kit` Git repo, and adjust the setup script to
       fork that. Configure a workflow to close all pull requests on that
       separate repo with the "wrong target" message.
    
    2. Rewrite the setup script so that rather than forking our repo, it
       instead creates a new one (not in a fork relationship with
       endlessm/threadbare) and pushes the latest such tag as its `main`.
    
    I have a fancier version of this patch which synthesises a merge commit
    between the commit that the kit is based on and the most recent commit
    on a `storyquest-kit` branch but I decided that the complexity isn't
    worth it unless we decide we want to go down one of those routes.

--- tools/sqckck.sh
+++ tools/sqckck.sh
@@ -2,8 +2,9 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
 set -ex
-: ${GODOT:=godot}
-pushd $(dirname "$(dirname "$0")")
+: "${GODOT:=godot}"
+: "${GITHUB_OUTPUT:=/dev/fd/1}"
+pushd "$(readlink -f "$(dirname "$0")/..")"
 
 VERSION=$(git describe --tags)
 declare -a PRUNE_FOLDERS=(
@@ -33,15 +34,15 @@ echo "::endgroup::"
 
 echo "::group::Committing pruned project"
 git switch --detach
-git commit --no-verify -am "Create StoryQuest kit for $VERSION"
+git add --update
+git commit --no-verify -m "Create StoryQuest kit for $VERSION"
+TAG_NAME="$VERSION-storyquest-kit"
+git tag --force -m "StoryQuest kit for $VERSION" "$TAG_NAME"
+echo "tag_name=$TAG_NAME" >> "$GITHUB_OUTPUT"
 echo "::endgroup::"
-
-# TODO: merge this to a special branch? Push this as a tag?
 
 echo "::group::Creating zip file"
 git archive --format=zip --prefix=threadbare-storyquest/ --output="threadbare-storyquest-$VERSION.zip" @
 echo "::endgroup::"
 
-if [[ -n "$GITHUB_OUTPUT" ]]; then
-  echo "zip_file=threadbare-storyquest-$VERSION.zip" >> "$GITHUB_OUTPUT"
-fi
+echo "zip_file=threadbare-storyquest-$VERSION.zip" >> "$GITHUB_OUTPUT"
