5152f2221eae3a9a90732d4ced40890d1d7b92b7
Threadbare StoryQuest Kit Maker <41898282+github-actions[bot]@users.noreply.github.com>
2026-08-04 20:04:43 +0000

    Create StoryQuest kit for v0.2.2

--- scenes/quests/story_quests/shjourney/0_IntroVideo/saltar_video.gd
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: The Threadbare Authors
-# SPDX-License-Identifier: MPL-2.0
-extends VideoStreamPlayer
-
-func _input(event):
-	if event.is_action_pressed(&"interact"):
-		SceneSwitcher.change_to_file(
-			"res://scenes/quests/story_quests/shjourney/1_IntroAnimacion/SleepingBros.tscn"
-		)
