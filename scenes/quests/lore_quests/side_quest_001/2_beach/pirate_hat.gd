7477e701d36eb7103c356a778a0777db4c1da87e
Francisco Ortega <franktega24@gmail.com>
2026-08-11 12:58:26 -0700

    dd Beach parrot and pirate hat quest
    Added the parrot and pirate hat interaction. The player can grapple the parrot to drop the hat, pick it up through dialogue, and return it to the skeleton pirate.

---
+++ scenes/quests/lore_quests/side_quest_001/2_beach/pirate_hat.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+var skeleton_pirate: Node = null
+
+func give_hat_to_skeleton() -> void:
+	if skeleton_pirate != null:
+		skeleton_pirate.got_hat = true
+
+	hide()
---
+++ scenes/quests/lore_quests/side_quest_001/2_beach/skeleton_pirate.gd
@@ -0,0 +1,3 @@
+extends Area2D
+
+var got_hat: bool = false
