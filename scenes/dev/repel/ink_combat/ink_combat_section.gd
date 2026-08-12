f297194704fb50289048612e60f7219898447031
felixwalberg <fwalberg@uvm.edu>
2026-08-12 15:06:19 -0400

    Add ink combat arena with multiple variations

---
+++ scenes/dev/repel/ink_combat/ink_combat_section.gd
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Node2D
+
+var _barrels: Array[FillingBarrel]
+var _enemies: Array[ThrowingEnemy]
+
+
+func _ready() -> void:
+	_update_barrels()
+	_update_enemies()
+
+
+## Store a copy of the FillinBarrel nodes before
+## any changes occur during ink combat.
+func _update_barrels() -> void:
+	for barrel: FillingBarrel in get_child(1).get_children():
+		var barrel_copy := barrel.duplicate()
+		_barrels.append(barrel_copy)
+
+
+## Store a copy of the ThrowingEnemy nodes before
+## any changes occur during ink combat.
+func _update_enemies() -> void:
+	_enemies = []
+	for enemy: ThrowingEnemy in get_child(0).get_children():
+		var enemy_copy := enemy.duplicate()
+		_enemies.append(enemy_copy)
+
+
+## Add fresh duplicate nodes to the scene tree and
+## corresponding groups to replace the ones depleted
+## in ink combat.
+func reset() -> void:
+	for enemy: ThrowingEnemy in get_child(0).get_children():
+		if is_instance_valid(enemy):
+			enemy.call_deferred("free")
+
+	for enemy: ThrowingEnemy in _enemies:
+		get_child(0).call_deferred("add_child", enemy)
+		enemy.add_to_group("throwing_enemy")
+
+	for barrel: FillingBarrel in get_child(1).get_children():
+		if is_instance_valid(barrel):
+			barrel.free()
+
+	for barrel: FillingBarrel in _barrels:
+		get_child(1).add_child(barrel)
+		barrel.add_to_group("filling_barrels")
+
+	_barrels = []
+	_update_barrels()
+	# Enemies must be deferred, to avoid errors
+	# with the attack animation flushing.
+	call_deferred("_update_enemies")
---
+++ scenes/dev/repel/ink_combat/ink_combat_test.gd
@@ -0,0 +1,75 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Node2D
+
+var _current_section: Node2D
+
+@onready var sections: Node2D = %Sections
+@onready var fill_game_logic: FillGameLogic = %FillGameLogic
+@onready var barrel_unlock_sequence: BarrelUnlockSequence = %BarrelUnlockSequence
+
+
+## Start the fill game of the first section
+func _ready() -> void:
+	_current_section = sections.get_child(0)
+	_set_group()
+	fill_game_logic.start()
+
+
+## Use index of section to add and remove ThrowingEnemy and
+## FillingBarrel from their groups before starting the
+## ink combat sequence.
+func set_section(selection: int) -> void:
+	_current_section = sections.get_child(selection)
+	for section in sections.get_children():
+		section.call_deferred("reset")
+
+	# Cooldown between reset and start
+	await get_tree().create_timer(2).timeout
+
+	_refresh_fill_logic()
+
+	fill_game_logic.call_deferred("_update_allowed_colors")
+	_set_group()
+
+	# The last section (index 3) uses BarrelUnlockSequence
+	if _current_section == sections.get_child(3):
+		barrel_unlock_sequence.barrels = []
+		for barrel in get_tree().get_nodes_in_group("filling_barrels"):
+			barrel_unlock_sequence.barrels.append(barrel)
+		barrel_unlock_sequence.start_sequence()
+
+	# Start up the throwing enemies remaining in the group
+	for enemy in get_tree().get_nodes_in_group("throwing_enemy"):
+		enemy.start()
+
+
+## Connect all "fresh" FillingBarrels to the FillGameLogic.
+## Uses same logic as _ready() for fill_game_logic.gd
+func _refresh_fill_logic() -> void:
+	fill_game_logic.barrels_to_win = _current_section.get_child(1).get_children().size()
+	fill_game_logic.barrels_completed = 0
+
+	var filling_barrels: Array = get_tree().get_nodes_in_group("filling_barrels")
+	fill_game_logic.barrels_to_win = clampi(
+		fill_game_logic.barrels_to_win, 0, filling_barrels.size()
+	)
+	for barrel: FillingBarrel in filling_barrels:
+		if not barrel.is_connected("completed", fill_game_logic._on_barrel_completed):
+			barrel.completed.connect(fill_game_logic._on_barrel_completed)
+
+
+## Remove all unused ThrowingEnemy and FillingBarrel nodes
+## from group, while adding the selected section.
+func _set_group() -> void:
+	for section: Node2D in sections.get_children():
+		if section != _current_section:
+			for throwing_enemy: ThrowingEnemy in section.get_child(0).get_children():
+				throwing_enemy.remove_from_group("throwing_enemy")
+			for barrel: FillingBarrel in section.get_child(1).get_children():
+				barrel.remove_from_group("filling_barrels")
+		else:
+			for throwing_enemy: ThrowingEnemy in section.get_child(0).get_children():
+				throwing_enemy.add_to_group("throwing_enemy")
+			for barrel: FillingBarrel in section.get_child(1).get_children():
+				barrel.add_to_group("filling_barrels")
