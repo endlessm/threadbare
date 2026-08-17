ab4d9a105a000249fc2a2738236a822f19650f20
felixwalberg <fwalberg@uvm.edu>
2026-08-17 16:50:09 -0400

    Add CharacterSpriteBehavior node to sheep.tscn

--- scenes/dev/components/sheep_chase.gd
+++ scenes/dev/components/sheep_chase.gd
@@ -8,15 +8,11 @@ extends Node2D
 ## How long the sheep's head start is.
 @export var head_start: float = 1.5
 
-var sheep_sprite: AnimatedSprite2D
-
-@onready var sheep_path_follow: CharacterBody2D = %SheepPathFollow
 @onready var sheep_path_walk_behavior: PathWalkBehavior = %SheepPathWalkBehavior
 @onready var townie_path_walk_behavior: PathWalkBehavior = %TowniePathWalkBehavior
 
 
 func _ready() -> void:
-	sheep_sprite = sheep_path_follow.get_node("AnimatedSprite2D")
 	# TODO: Give sheep a walk animation
 	sheep_path_walk_behavior.speeds.walk_speed = speed
 
@@ -24,8 +20,3 @@ func _ready() -> void:
 	await get_tree().create_timer(head_start).timeout
 
 	townie_path_walk_behavior.speeds.walk_speed = speed
-
-
-## Update the direction sheep sprite faces based on velocity.
-func _process(_delta: float) -> void:
-	sheep_sprite.flip_h = sheep_path_follow.velocity.x < 0
