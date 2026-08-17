6a1b3766b149fe8509f7f95063032283fae9cc63
Francisco Ortega <franktega24@gmail.com>
2026-08-17 11:45:24 -0700

    Add Beach skeleton and hat interation
    Added the skeleton pirate interaction, hat pickup dialogue, journal entry, and updated Beach scene. Added logic for returning the pirate hat and changing the skeleton's appearance.

--- scenes/quests/lore_quests/side_quest_001/2_beach/pirate_hat.gd
+++ scenes/quests/lore_quests/side_quest_001/2_beach/pirate_hat.gd
@@ -5,5 +5,6 @@ var skeleton_pirate: Node = null
 func give_hat_to_skeleton() -> void:
 	if skeleton_pirate != null:
 		skeleton_pirate.got_hat = true
+		skeleton_pirate.put_hat_on()
 
 	hide()
--- scenes/quests/lore_quests/side_quest_001/2_beach/skeleton_pirate.gd
+++ scenes/quests/lore_quests/side_quest_001/2_beach/skeleton_pirate.gd
@@ -1,3 +1,14 @@
 extends Area2D
 
 var got_hat: bool = false
+@export var exit_blocker: StaticBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func put_hat_on() -> void:
+	got_hat = true
+	animated_sprite.play("Skeleton_withhat")
+	
+func remove_exit_blocker() -> void:
+	if exit_blocker != null:
+		exit_blocker.queue_free()
