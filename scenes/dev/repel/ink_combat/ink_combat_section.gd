5cfe6cae30d41aeb09865e2a117284d04910e8f9
felixwalberg <fwalberg@uvm.edu>
2026-08-19 08:54:37 -0400

    Duplicate enemy container rather than replacing each iteration

--- scenes/dev/repel/ink_combat/ink_combat_section.gd
+++ scenes/dev/repel/ink_combat/ink_combat_section.gd
@@ -14,22 +14,9 @@ var _enemies: Array[ThrowingEnemy]
 
 
 func _ready() -> void:
-	_update_barrels()
-	_update_enemies()
-
-
-## Store a copy of the FillinBarrel nodes before
-## any changes occur during ink combat.
-func _update_barrels() -> void:
 	for barrel: FillingBarrel in barrel_container.get_children():
 		var barrel_copy := barrel.duplicate()
 		_barrels.append(barrel_copy)
-
-
-## Store a copy of the ThrowingEnemy nodes before
-## any changes occur during ink combat.
-func _update_enemies() -> void:
-	_enemies = []
 	for enemy: ThrowingEnemy in enemy_container.get_children():
 		var enemy_copy := enemy.duplicate()
 		_enemies.append(enemy_copy)
@@ -41,22 +28,20 @@ func _update_enemies() -> void:
 func reset() -> void:
 	for enemy: ThrowingEnemy in enemy_container.get_children():
 		if is_instance_valid(enemy):
+			enemy_container.remove_child(enemy)
+			enemy.queue_free()
 			enemy.call_deferred("free")
 
 	for enemy: ThrowingEnemy in _enemies:
-		enemy_container.call_deferred("add_child", enemy)
-		enemy.add_to_group("throwing_enemy")
+		var new_enemy := enemy.duplicate()
+		enemy_container.call_deferred("add_child", new_enemy)
+		new_enemy.add_to_group("throwing_enemy")
 
 	for barrel: FillingBarrel in barrel_container.get_children():
 		if is_instance_valid(barrel):
 			barrel.free()
 
 	for barrel: FillingBarrel in _barrels:
-		barrel_container.add_child(barrel)
-		barrel.add_to_group("filling_barrels")
-
-	_barrels = []
-	_update_barrels()
-	# Enemies must be deferred, to avoid errors
-	# with the attack animation flushing.
-	call_deferred("_update_enemies")
+		var new_barrel := barrel.duplicate()
+		barrel_container.add_child(new_barrel)
+		new_barrel.add_to_group("filling_barrels")
