5e8f0b501ca10f35ad8e6129e65f8ec3ec24cc07
Renee <reneecardozaleon@gmail.com>
2026-08-19 14:06:45 -0700

    Cleaned Up Formatting of Code to Pass Checks (#2756)
    Changed the order of variables and added appropriate whitespaces to follow formatting guidelines.

--- scenes/quests/lore_quests/side_quest_001/2_beach/skeleton_pirate.gd
+++ scenes/quests/lore_quests/side_quest_001/2_beach/skeleton_pirate.gd
@@ -1,14 +1,19 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
 extends Area2D
 
-var got_hat: bool = false
 @export var exit_blocker: StaticBody2D
+
+var got_hat: bool = false
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func put_hat_on() -> void:
 	got_hat = true
 	animated_sprite.play("Skeleton_withhat")
-	
+
+
 func remove_exit_blocker() -> void:
 	if exit_blocker != null:
 		exit_blocker.queue_free()
--- scenes/quests/lore_quests/side_quest_001/components/skeleton/skeleton_movement.gd
+++ scenes/quests/lore_quests/side_quest_001/components/skeleton/skeleton_movement.gd
@@ -7,23 +7,25 @@ extends StaticBody2D
 @export var start_dialogue: DialogueResource
 @export var end_dialogue: DialogueResource
 
-@onready var talk_behavior: TalkBehavior = $TalkBehavior
-
 var already_teleported: bool = false
 
-# Called when the node enters the scene tree for the first time.
+@onready var talk_behavior: TalkBehavior = $TalkBehavior
+
+
+## Sets the starting position and dialogue of the skeleton.
 func _ready() -> void:
 	position = start_position
 	talk_behavior.dialogue = start_dialogue
 
 
-## When a puzzle is solved, this changes the skeleton's position and their dialogue
-func puzzle_solved():
+## When a puzzle is solved, this changes the skeleton's position and their dialogue.
+func puzzle_solved() -> void:
 	if not already_teleported:
 		position = end_position
 		talk_behavior.dialogue = end_dialogue
 		already_teleported = true
 
 
-func _on_interaction_started(player: Player, from_right: bool) -> void:
+## When the player begins to read the bool, call the function that moves the skeleton.
+func _on_interaction_started(_player: Player, _from_right: bool) -> void:
 	puzzle_solved()
