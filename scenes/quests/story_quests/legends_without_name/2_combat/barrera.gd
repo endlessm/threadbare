985c1f90395e86739e65d6c6c507aa4a0ad94ab3
Karm <benjamin.jk1408@gmail.com>
2026-08-24 15:51:43 -0500

    Arreglo de barrera al acabar con el enemigo nvl2

--- scenes/quests/story_quests/legends_without_name/2_combat/barrera.gd
+++ scenes/quests/story_quests/legends_without_name/2_combat/barrera.gd
@@ -2,9 +2,20 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
 
-func ocultar_y_desactivar() -> void:
-	# Oculta el nodo Barrera 
-	hide() 
+@export var game_logic: Node
+
+func _ready() -> void:
+	if is_instance_valid(game_logic) and game_logic.has_signal("goal_reached"):
+		game_logic.goal_reached.connect(_on_puzzle_completado)
+
+func _on_puzzle_completado() -> void:
+	hide() # Oculta la barrera y sus sprites
 	
-	# Desactiva la colisión 
-	$StaticBody2D/CollisionShape2D.set_deferred("disabled", true)
+	var static_body: StaticBody2D = $StaticBody2D
+	if is_instance_valid(static_body):
+		static_body.collision_layer = 0
+		static_body.collision_mask = 0
+		
+	var colision: Node = $StaticBody2D.get_child(0)
+	if is_instance_valid(colision):
+		colision.set_deferred("disabled", true)
