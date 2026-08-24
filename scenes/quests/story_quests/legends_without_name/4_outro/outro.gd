9218d2f5dc95dcd78cd5aa963c1284d71632cd27
Karm <benjamin.jk1408@gmail.com>
2026-08-23 20:10:15 -0500

    arreglo outro dialogo

--- scenes/quests/story_quests/legends_without_name/4_outro/outro.gd
+++ scenes/quests/story_quests/legends_without_name/4_outro/outro.gd
@@ -2,14 +2,16 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Control
 
-
+@export var dialogue: DialogueResource = preload("uid://b7ad8nar1hmfs")
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
 	$AnimationPlayer.play("outro")
 
 	pass  # Replace with function body.
+	DialogueManager.show_dialogue_balloon(dialogue, "", [self])
 	await $AnimationPlayer.animation_finished
+	await DialogueManager.dialogue_ended
 	get_tree().change_scene_to_file("res://scenes/world_map/frays_end.tscn")
 
 
