5d24b630bbf7eeb40dd419703ca400bb60d32b9b
Salonso928 <alonso89benito2@gmail.com>
2026-08-26 16:58:38 -0500

    Script delete blocks cambiado, ahora tambien coloca muros, pequeño rediseño al mapa nivel 1

--- scenes/quests/story_quests/la_odisea_del_tiempo/1_stealth/delete_blocks.gd
+++ scenes/quests/story_quests/la_odisea_del_tiempo/1_stealth/delete_blocks.gd
@@ -4,11 +4,19 @@ extends Node2D
 @onready var stones = $Stone;
 
 var bloques_a_borrar=[
-	Vector2i(44,27),
 	Vector2i(44,28),
 	Vector2i(44,29),
 	Vector2i(44,30),
 ]
+var bloques_a_colocar=[
+	Vector2i(51,28),
+	Vector2i(51,29),
+	Vector2i(51,30),
+]
 func delete_stones()->void:
 	for b in bloques_a_borrar:
 		stones.set_cell(b,-1);
+func put_stones()->void:
+	for b in bloques_a_colocar:
+		stones.set_cell(b,0,Vector2i(1,1),0)
+		
