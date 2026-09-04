9035f0522b73a462d262ba1f833a8e99c4fbf977
renato-sy <110872024+renato-sy@users.noreply.github.com>
2026-09-04 09:37:35 -0500

    Fix linting errors in area_name.gd: remove trailing whitespace and reorder definitions

--- scenes/ui_elements/area_name/components/area_name.gd
+++ scenes/ui_elements/area_name/components/area_name.gd
@@ -8,14 +8,14 @@ const MAX_CARACTERES_POR_LINEA: int = 10
 @export_multiline var zone_name: String:
 	set(valor):
 		var lineas: PackedStringArray = valor.split("\n")
-		
+
 		if lineas.size() > MAX_LINEAS:
 			lineas = lineas.slice(0, MAX_LINEAS)
-		
+
 		for i in range(lineas.size()):
 			if lineas[i].length() > MAX_CARACTERES_POR_LINEA:
 				lineas[i] = lineas[i].substr(0, MAX_CARACTERES_POR_LINEA)
-		
+
 		_zone_name = "\n".join(lineas)
 	get:
 		return _zone_name
@@ -23,7 +23,7 @@ const MAX_CARACTERES_POR_LINEA: int = 10
 @export_range(1, 3) var time: int = 2
 
 var _zone_name: String = ""
-var is_area = null 
+var is_area = null
 var entered: bool = false
 
 func _ready() -> void:
@@ -41,23 +41,23 @@ func detect_active_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and entered:
 		# Si is_area es null, significa que ya hay una animación ejecutándose. Bloqueamos.
 		if is_area == null:
-			return 
+			return
 
-		entered = false 
-		
+		entered = false
+
 		if is_area == false:
-			is_area = null 
-			
+			is_area = null
+
 			var temp: Control = preload("res://scenes/ui_elements/area_name/first_unlock.tscn").instantiate()
 			$CanvasLayer.add_child(temp)
-			
+
 			# Desbloqueamos en el GameState global
 			GameState.global.set_unlock_area(_zone_name)
 			GameState.save()
-			
+
 			await temp.animate_first_unlock(zone_name, time)
 			temp.queue_free()
-			
+
 			$Timer.start()
 
 		elif is_area == true:
