4d061809a5ceb54e182c169d6d39081d46d3375e
Manuel Quiñones <manuq@endlessaccess.org>
2026-09-04 15:57:27 -0300

    ThreadbareEditor addon: Add translation parser for quests
    So the .tres file is recognized by the translation system, and title and
    description of Quests can be extracted to the catalog.

--- addons/threadbare_editor/plugin.gd
+++ addons/threadbare_editor/plugin.gd
@@ -5,16 +5,27 @@ class_name ThreadbareEditorPlugin
 extends EditorPlugin
 
 const ThreadbareDebugger := preload("res://addons/threadbare_editor/threadbare_debugger.gd")
+const QuestTranslationParser := preload(
+	"res://addons/threadbare_editor/quest_translation_parser.gd"
+)
 
 var threadbare_debugger: EditorDebuggerPlugin
+var quest_translation_parser: EditorTranslationParserPlugin
 
 
 func _enter_tree() -> void:
 	threadbare_debugger = ThreadbareDebugger.new()
 	add_debugger_plugin(threadbare_debugger)
 
+	quest_translation_parser = QuestTranslationParser.new()
+	add_translation_parser_plugin(quest_translation_parser)
+
 
 func _exit_tree() -> void:
 	if threadbare_debugger:
 		remove_debugger_plugin(threadbare_debugger)
 		threadbare_debugger = null
+
+	if quest_translation_parser:
+		remove_translation_parser_plugin(quest_translation_parser)
+		quest_translation_parser = null
