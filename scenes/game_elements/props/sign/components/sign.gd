a3f527cc9d8548b0ddee7951cf0a16cfbdbda0da
Joshua Jenner <joshua-jenner@hotmail.com>
2026-09-20 18:22:54 +0800

    Updated sprite and update_appearance

--- scenes/game_elements/props/sign/components/sign.gd
+++ scenes/game_elements/props/sign/components/sign.gd
@@ -27,7 +27,12 @@ func _ready() -> void:
 
 
 func update_appearance() -> void:
-	$Appearance.flip_h = direction == Enums.LookAtSide.RIGHT
+	if direction == Enums.LookAtSide.UNSPECIFIED:
+		$Appearance.frame = 0
+		$Appearance.flip_h = false
+	else:
+		$Appearance.frame = 1
+		$Appearance.flip_h = direction == Enums.LookAtSide.RIGHT
 
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
