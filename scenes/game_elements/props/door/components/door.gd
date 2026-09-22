3718c9a60dcd92e386e317a2d0d474a3b0a406a2
Manuel Quiñones <manuq@endlessaccess.org>
2026-09-22 10:47:52 -0300

    Update scenes/game_elements/props/door/components/door.gd

--- scenes/game_elements/props/door/components/door.gd
+++ scenes/game_elements/props/door/components/door.gd
@@ -44,7 +44,6 @@ func update_opened_state() -> void:
 
 func update_door_flip() -> void:
 	%DoorOpened.flip_h = flip_horizontal 
-	
 	if flip_horizontal:
 		%DoorOpened.position.x = -abs(%DoorOpened.position.x)
 	else:
