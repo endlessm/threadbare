ade75319e938549ffa59f084c90e482ac32f7dce
Manuel Quiñones <manuq@endlessaccess.org>
2026-09-22 10:46:59 -0300

    Update scenes/game_elements/props/door/components/door.gd

--- scenes/game_elements/props/door/components/door.gd
+++ scenes/game_elements/props/door/components/door.gd
@@ -40,7 +40,7 @@ func update_opened_state() -> void:
 
 	%ColliderWhenClosed.set_collision_layer_value(Enums.CollisionLayers.WALLS, not opened)
 	%ColliderWhenClosed.set_collision_mask_value(Enums.CollisionLayers.PLAYERS, not opened)
-	
+
 
 func update_door_flip() -> void:
 	%DoorOpened.flip_h = flip_horizontal 
