b239d2314eb480cbc7ec74ff42c29912240a7b64
GarayLlajaLeonardo <bruno951357@gmail.com>
2026-09-23 01:55:10 -0500

    Fix linting and code formatting

--- scenes/game_elements/props/decoration/sheep/face_away_behavior.gd
+++ scenes/game_elements/props/decoration/sheep/face_away_behavior.gd
@@ -3,8 +3,6 @@
 extends Node
 
 func apply(player: Player, pet_animation: AnimatedSprite2D) -> void:
-	var player_is_on_right := (
-	player.global_position.x > pet_animation.global_position.x
-	)
+	var player_is_on_right := player.global_position.x > pet_animation.global_position.x
 	var parent_is_flipped := pet_animation.global_transform.x.x < 0.0
 	pet_animation.flip_h = player_is_on_right != parent_is_flipped
