0bc0f4507c5dccd8917273ba00f526a8276ca7f6
MateoP <unplayersacc@gmail.com>
2026-09-23 16:49:17 -0500

    balloon instead of addon

--- scenes/ui_elements/dialogue/components/text_effects/text_effect_imagination.gd
+++ scenes/ui_elements/dialogue/components/text_effects/text_effect_imagination.gd
@@ -1,23 +1,20 @@
 @tool
-# Having a class name is handy for picking the effect in the Inspector.
 class_name ImaginationTextEffect
 extends RichTextEffect
 
-# To use this effect:
-# - Use [Memory]hello[/Memory] in text.
 var bbcode := "Imagination"
+
+var text_color := Color("#7C6000")
+var text_markup := Color("c49f23ff")
+
+var speed: float = 4.0
+var span: float = 2.0
 
 
 func _process_custom_fx(char_fx: CharFXTransform) -> bool:
-	var text_color := Color("#7C6000")
-	var text_markup := Color("c49f23ff")
+	var time_var := sin(char_fx.elapsed_time * speed + (char_fx.range.x / span)) * 0.5 + 0.5
 
-	var speed: float = 4.0
-	var span: float = 2.0
+	char_fx.color = text_color.lerp(text_markup, time_var)
+	char_fx.offset = Vector2(0, -time_var * 2)
 
-	# Cálculo de la onda seno suave entre 0.0 y 1.0 basada en el tiempo y la posición
-	var factor_seno = sin(char_fx.elapsed_time * speed + (char_fx.range.x / span)) * 0.5 + 0.5
-
-	char_fx.color = text_color.lerp(text_markup, factor_seno)
-	char_fx.offset = Vector2(0, -factor_seno * 2)
 	return true
