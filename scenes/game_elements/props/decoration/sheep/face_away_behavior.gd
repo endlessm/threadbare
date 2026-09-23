ca7c3a0dd3e4e0ca6a9f73e991d891cced906259
GarayLlajaLeonardo <bruno951357@gmail.com>
2026-09-22 23:42:56 -0500

    pet and cat animations

---
+++ scenes/game_elements/props/decoration/sheep/face_away_behavior.gd
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Node
+
+func apply(player: Player, pet_animation: AnimatedSprite2D) -> void:
+	var player_is_on_right := (
+	player.global_position.x > pet_animation.global_position.x
+	)
+	var parent_is_flipped := pet_animation.global_transform.x.x < 0.0
+	pet_animation.flip_h = player_is_on_right != parent_is_flipped
