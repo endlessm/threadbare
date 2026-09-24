eb387d7912d13f778ac6b8e0013fa8719ceb8136
renato-sy <renatorupaylla20@gmail.com>
2026-09-23 21:24:52 -0500

    Refactor zone name handling: standardize variable names and update scene files for consistency

--- scenes/ui_elements/area_name/components/area_name.gd
+++ scenes/ui_elements/area_name/components/area_name.gd
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
 
-@export_multiline var zone_name: String
+@export var zone_name_text: String
 
 @export_range(1, 3) var time: int = 2
 
@@ -11,10 +11,10 @@ func _on_detect_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("player"):
 		return
 
-	if GameState.global.current_area_name == zone_name:
+	if GameState.global.current_area_name == zone_name_text:
 		return
 
-	GameState.global.current_area_name = zone_name
+	GameState.global.current_area_name = zone_name_text
 
-	GameState.global.set_unlock_area(zone_name)
+	GameState.global.set_unlock_area(zone_name_text)
 	GameState.save()
