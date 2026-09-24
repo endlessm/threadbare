e10478fb41dbf0b350a578942b9a532f1a3df533
Threadbare StoryQuest Kit Maker <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-24 20:06:40 +0000

    Create StoryQuest kit for v0.2.3

--- scenes/quests/story_quests/shjourney/1_IntroAnimacion/saltar_escena.gd
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: The Threadbare Authors
-# SPDX-License-Identifier: MPL-2.0
-extends AnimatedSprite2D
-
-func _input(event):
-	if event.is_action_pressed("interact"):
-		SceneSwitcher.change_to_file("res://scenes/quests/story_quests/shjourney/2_shjourney_intro/template_intro.tscn")
--- scenes/quests/story_quests/shjourney/9_shjourney_outro_2/character_2.gd
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: The Threadbare Authors
-# SPDX-License-Identifier: MPL-2.0
-extends Node2D # o KinematicBody2D según sea tu nodo padre
